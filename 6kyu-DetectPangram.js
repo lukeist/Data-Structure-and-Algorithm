b6ed25a862e76405b56795720a7abb8fcbae3352
hiluan <apt.muenchen@gmail.com>
2021-08-14 21:22:00 +0100

    detect pangram: use array.every()

--- 6kyu-DetectPangram.js
+++ 6kyu-DetectPangram.js
@@ -5,19 +5,27 @@
 // e: '"The quick brown fox jumps over the lazy dog."  => T
 // 'asdlfasdf' => F
 
+// function isPangram(string) {
+//   let alphabet = "abcdefghijklmnopqrstuvwxyz";
+//   // convert string to lowercase()
+//   string = string.toLowerCase();
+//   console.log(string);
+//   // loop through string, check if each character is in alphabet
+//   // remove matched char from alphabet
+//   for (char of string) {
+//     alphabet.includes(char) && (alphabet = alphabet.replace(char, ""));
+//     console.log(alphabet.includes(char), alphabet);
+//   }
+//   // conditionals: if alphabet length === 0 return true
+//   return alphabet.length === 0;
+// }
+
+isPangram("The quick brown fox jumps over the lazy dog");
+
+// better:
 function isPangram(string) {
   let alphabet = "abcdefghijklmnopqrstuvwxyz";
   // convert string to lowercase()
   string = string.toLowerCase();
-  console.log(string);
-  // loop through string, check if each character is in alphabet
-  // remove matched char from alphabet
-  for (char of string) {
-    alphabet.includes(char) && (alphabet = alphabet.replace(char, ""));
-    console.log(alphabet.includes(char), alphabet);
-  }
-  // conditionals: if alphabet length === 0 return true
-  return alphabet.length === 0;
+  return alphabet.split("").every((c) => string.includes(c));
 }
-
-isPangram("The quick brown fox jumps over the lazy dog");
