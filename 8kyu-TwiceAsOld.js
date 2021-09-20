d417d9acae652a71bb136b0ec8572b26c253dda4
hiluan <apt.muenchen@gmail.com>
2021-09-20 13:33:00 +0100

    twice as old

---
+++ 8kyu-TwiceAsOld.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  return Math.abs(sonYearsOld * 2 - dadYearsOld);
+}
