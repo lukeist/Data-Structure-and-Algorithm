c79de95e4462b0ca54bede5c5a11166624cbd40b
hiluan <apt.muenchen@gmail.com>
2021-10-17 06:11:00 +0100

    leetcode 21 merge two sorted list

--- leetcode--21-MergeTwoSortedLists.js
+++ leetcode--21-MergeTwoSortedLists.js
@@ -21,39 +21,44 @@ class ListNode {
   }
 }
 
+var mergeTwoLists = function (l1, l2) {
+  let newList = new ListNode();
+  let tail = newList;
+
+  // 2 ------ 3
+
+  // 1 ------ 4
+
+  // nL ------->  1 --------> 2 --------> 3 --------> 4
+  // 0            T   t.n    T
+
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      tail.next = l2;
+      tail = l2;
+      l2 = l2.next;
+    } else {
+      tail.next = l1;
+      tail = l1;
+      l1 = l1.next;
+    }
+  }
+
+  !l1 && (tail.next = l2);
+  !l2 && (tail.next = l1);
+
+  console.log(l1, l2);
+  // console.log(tail);
+  // console.log(newList);
+  console.log(newList.next.next.next.next);
+
+  return newList;
+};
+
 // l
 // 1 1
 // l1  1-  2   4
 // l2  1   3   4
-
-var mergeTwoLists = function (l1, l2) {
-  let newList = new ListNode();
-  let prev = newList;
-  let count = 0;
-  while (l1 && l2) {
-    count++;
-    console.log(newList);
-
-    if (l1.val > l2.val) {
-      prev.next = l2;
-      prev = l2;
-      // console.log(prev);
-
-      l2 = l2.next;
-    } else {
-      prev.next = l1;
-      prev = l1;
-
-      l1 = l1.next;
-    }
-    console.log(newList);
-    console.log("-------------------------");
-
-    // console.log(count, prev);
-  }
-
-  // console.log(newList);
-};
 
 const a = new ListNode(1);
 const b = new ListNode(2);
@@ -68,3 +73,32 @@ d.next = e;
 e.next = f;
 
 mergeTwoLists(a, d);
+
+// var mergeTwoLists = function (l1, l2) {
+//   let newList = new ListNode();
+//   let prev = newList;
+//   let count = 0;
+//   while (l1 && l2) {
+//     count++;
+//     console.log(newList);
+
+//     if (l1.val > l2.val) {
+//       prev.next = l2;
+//       prev = l2;
+//       // console.log(prev);
+
+//       l2 = l2.next;
+//     } else {
+//       prev.next = l1;
+//       prev = l1;
+
+//       l1 = l1.next;
+//     }
+//     console.log(newList);
+//     console.log("-------------------------");
+
+//     // console.log(count, prev);
+//   }
+
+//   // console.log(newList);
+// };
