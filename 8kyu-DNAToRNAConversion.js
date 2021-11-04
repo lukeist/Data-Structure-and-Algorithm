d336c246293aa05ff8886521e9ea02a6d565fced
hiluan <apt.muenchen@gmail.com>
2021-11-04 17:32:00 +0100

    dna to rna conversion

---
+++ 8kyu-DNAToRNAConversion.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5556282156230d0e5e000089/train/javascript
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  return dna
+    .split("")
+    .map((v) => (v === "T" ? "U" : v))
+    .join("");
+}
