cb22c51bc2aed01a0b8d90202f42093e182be955
hiluan <apt.muenchen@gmail.com>
2021-11-20 15:16:00 +0100

    number of people in the bus

--- 7kyu-NumberOfPeopleInTheBus.js
+++ 7kyu-NumberOfPeopleInTheBus.js
@@ -1,5 +1,15 @@
 // https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/javascript
+// P: arr of arrays of ints
+// r: num int +
+// e:
+// [[10,0],[3,5],[5,8]]) => 5
 
 var number = function (busStops) {
-  // Good Luck!
+  // get in bus = sum all arr[0]
+  // get out bus = sum all arr[1]
+  // last ppl on bus = sum arr[0] - sum arr[1]
+  return (
+    busStops.reduce((p, c) => p + c[0], 0) -
+    busStops.reduce((p, c) => p + c[1], 0)
+  );
 };
