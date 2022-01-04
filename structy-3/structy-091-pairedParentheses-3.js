f1076e8162fffed5ed22263d8779035f2fb02678
hiluan <apt.muenchen@gmail.com>
2022-01-04 22:34:00 +0100

    stack - O(n) O(n) & O(n) O(1)

--- structy-3/structy-091-pairedParentheses-3.js
+++ structy-3/structy-091-pairedParentheses-3.js
@@ -11,3 +11,16 @@ const pairedParentheses = (str) => {
 
   return stack.length === 0;
 };
+
+// const pairedParentheses = (s) => {
+//   let count = 0;
+//   for (let c of s) {
+//     if (count === -1) return false;
+//     if (c === '(') {
+//       count++;
+//     } else if (c === ')') {
+//       count--;
+//     }
+//   }
+//   return count === 0;
+// }
