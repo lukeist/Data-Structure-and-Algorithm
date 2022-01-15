3356d91d193abb910825fa52624928e3d8bc823e
hiluan <apt.muenchen@gmail.com>
2022-01-15 08:42:00 +0100

    count of positives / sum of negatives

--- 8kyu-CountOfPositivesSumOfNegatives.js
+++ 8kyu-CountOfPositivesSumOfNegatives.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/576bb71bbbcf0951d5000044
 
 function countPositivesSumNegatives(input) {
-  if (input.length < 1) return [];
+  if (input === null || input.length < 1) return [];
   return [
     input.filter((n) => n > 0).length,
     input.filter((n) => n < 0).reduce((p, c) => p + c, 0),
