9d6f4f20d62d81d1413a4bbaf9c6b63f7ebdd040
hiluan <apt.muenchen@gmail.com>
2022-02-04 05:42:00 +0100

    returning strings

---
+++ 8kyu-ConvertAStringToANumber.js
@@ -0,0 +1,6 @@
+// 8kyu-ConvertAStringToANumber.js
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+const stringToNumber = function (str) {
+  // put your code here
+  return +str;
+};
---
+++ 8kyu-ReturningStrings.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+function greet(name) {
+  //your code here
+  return `Hello, ${name} how are you doing today?`;
+}
