b80ca79cc427212df6f42227b3c2f27bf2cb6ffe
hiluan <apt.muenchen@gmail.com>
2022-03-06 11:32:00 +0100

    'Product of consecutive Fib Numbers'

--- 5kyu-ProductOfConsecutiveFibNumbers.js
+++ 5kyu-ProductOfConsecutiveFibNumbers.js
@@ -1,25 +1,40 @@
 // https://www.codewars.com/kata/5541f58a944b85ce6d00006a/train/javascript
 
+// function productFib(prod) {
+//   // ...
+//   let arrFib = [0, 1];
+//   let numFib = 0;
+//   for (let i = 2; i <= arrFib.length; i++) {
+//     numFib = arrFib[i - 2] + arrFib[i - 1];
+//     numFib * arrFib[i - 1] < prod && arrFib.push(numFib);
+//     console.log(arrFib.length, numFib, i);
+//   }
+//   const lastNumFib = arrFib[arrFib.length - 1];
+//   const lastNumFibPlus1 = lastNumFib + arrFib[arrFib.length - 2];
+//   const arrTrue = [lastNumFib, lastNumFibPlus1, true];
+//   const arrFalse = [lastNumFib, lastNumFibPlus1, false];
+
+//   console.log(prod % lastNumFib === 0 ? arrTrue : arrFalse);
+// }
+// // productFib(714);
+// // productFib(447577); //[610, 987, false]
+// productFib(84049690); //[10946, 17711, false]
+
+// better solution:
 function productFib(prod) {
-  // ...
-  const sqrProd = Math.sqrt(prod);
-  let arrFib = [0, 1];
-  let numFib = 0;
-  for (let i = 2; i <= arrFib.length; i++) {
-    numFib = arrFib[i - 2] + arrFib[i - 1];
-    numFib < sqrProd && arrFib.push(numFib);
-    console.log(arrFib.length, numFib, i);
+  let n = 0;
+  let nPlus = 1;
+  while (n * nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
   }
-  const lastNumFib = arrFib[arrFib.length - 1];
-  const lastNumFibPlus1 = lastNumFib + arrFib[arrFib.length - 2];
-  const lastNumFibPlus2 = lastNumFibPlus1 + lastNumFib;
-
-  console.log(lastNumFib, lastNumFibPlus1, lastNumFibPlus2);
+  console.log(n, nPlus, prod % n === 0);
 }
+// productFib(447577); //[610, 987, false]
+productFib(714);
+// productFib(84049690); //[10946, 17711, false]
 
-// productFib(714);
-productFib(800);
-
+//////////////////////////////////////////////////////////////////// bad solutions:
 // function productFib(prod) {
 //     // ...
 //     const sqrProd = Math.sqrt(prod);
@@ -48,4 +63,30 @@ productFib(800);
 //           arrFib[arrFib.length - 1] + prod / arrFib[arrFib.length - 1],
 //           false,
 //         ];
+//   }
+
+// function productFib(prod) {
+//     // ...
+//     const sqrProd = Math.sqrt(prod);
+//     let arrFib = [0, 1];
+//     let numFib = 0;
+//     for (let i = 2; i <= arrFib.length; i++) {
+//       numFib = arrFib[i - 2] + arrFib[i - 1];
+//       numFib < sqrProd && arrFib.push(numFib);
+//       console.log(arrFib.length, numFib, i);
+//     }
+//     const lastNumFib = arrFib[arrFib.length - 1];
+//     const lastNumFibPlus1 = lastNumFib + arrFib[arrFib.length - 2];
+//     const lastNumFibPlus2 = lastNumFibPlus1 + lastNumFib;
+//     const arrTrue = [lastNumFib, lastNumFibPlus1, true];
+//     const arrFalse =
+//       sqrProd < lastNumFibPlus1
+//         ? [lastNumFib, lastNumFibPlus1, false]
+//         : [lastNumFibPlus1, lastNumFibPlus2, false];
+
+//     console.log(sqrProd);
+//     console.log(prod % lastNumFib === 0 ? arrTrue : arrFalse);
+//     return prod % lastNumFib === 0 ? arrTrue : arrFalse;
+
+//     //   console.log(lastNumFib, lastNumFibPlus1, lastNumFibPlus2);
 //   }
