4f85ab81418b7b4bd40cbd4b74ca4469ba951ebe
hiluan <apt.muenchen@gmail.com>
2022-04-08 19:12:00 +0100

    'Where My Anagrams At'

--- 5kyu-WhereMyAnagramsAt.js
+++ 5kyu-WhereMyAnagramsAt.js
@@ -1,14 +1,33 @@
 // https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/javascript
 
-function anagrams(word, words) {
-  let test = [];
-  for (value of words) {
-    for (char of word.split("")) {
-      test.push(value.split("").filter((v) => v === char));
-    }
-  }
-  console.log(test);
-}
+// function anagrams(word, words) {
+//   words = words.filter((v) => v.length === word.length);
+//   let anaArr = [];
+//   for (w of words) {
+//     w = w.split("");
+//     let tempStr = "";
+//     let wordClone = word;
+//     for (c of w) {
+//       wordClone.indexOf(c) > -1 && (tempStr += c);
+//       wordClone = wordClone.replace(c, "");
+//     }
+//     tempStr.length === word.length && anaArr.push(tempStr);
+//   }
 
+//   console.log(anaArr);
+//   return anaArr;
+// }
+
+/// better solutoion: use array.SORT()
+function anagrams(word, words) {
+  console.log(
+    words.filter(
+      (w) => w.split("").sort().join("") === word.split("").sort().join("")
+    )
+  );
+  return words.filter(
+    (w) => w.split("").sort().join("") === word.split("").sort().join("")
+  );
+}
 anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"]);
 //['carer', 'racer']
