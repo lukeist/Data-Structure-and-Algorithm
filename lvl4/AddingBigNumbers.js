3d1cee055a9378930efcf32d0abc85dfed15b47f
hiluan <apt.muenchen@gmail.com>
2022-04-15 22:31:19 -0700

    adding big number testing

---
+++ lvl4/AddingBigNumbers.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  let newA = [];
+  let newB = [];
+  // if (a.length > 17 ) {
+  //   for (let i = 0 ; i< a.length/17; i++
+
+  //let an = BigInt(a) +  BigInt(b)
+  //   console.log((Number(a) + Number(b)).toString()); // Fix me!
+  console.log(+a, +b);
+}
+
+add("12345678901234567", "0");
