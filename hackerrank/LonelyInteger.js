ee7297f1028e25fbc32271a857207655e21131da
hiluan <apt.muenchen@gmail.com>
2022-04-26 21:28:20 -0700

    Lonely Integer

--- hackerrank/LonelyInteger.js
+++ hackerrank/LonelyInteger.js
@@ -24,19 +24,33 @@
 // It is guaranteed that  is an odd number and that there is one unique element.
 // , where .
 
+// function lonelyinteger(arr) {
+//   // Write your code here
+//   for (let i = 0; i < arr.length; i++) {
+//     // console.log("this", arr[i]);
+//     const indexes = arr.reduce((containArr, c) => {
+//       arr[i] === c && containArr.push(c);
+//       return containArr;
+//     }, []);
+//     if (indexes.length === 1) {
+//       console.log(indexes[0]);
+//       return indexes[0];
+//     }
+//   }
+// }
+
+// better solution
 function lonelyinteger(arr) {
   // Write your code here
-  for (let i = 0; i < arr.length; i++) {
-    // console.log("this", arr[i]);
-    const indexes = arr.reduce((containArr, c) => {
-      arr[i] === c && containArr.push(c);
-      return containArr;
-    }, []);
-    if (indexes.length === 1) {
-      console.log(indexes[0]);
-      return indexes[0];
-    }
-  }
+  const newA = arr.filter((num) => arr.indexOf(num) === arr.lastIndexOf(num));
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   // console.log("this", arr[i]);
+  //   arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num)
+  //     // arr.indexOf(arr[i] === arr.lastIndexOf(arr[i]))
+  console.log(newA);
+
+  // }
 }
 
 lonelyinteger([1, 2, 3, 4, 3, 4, 9, 2, 1]);
