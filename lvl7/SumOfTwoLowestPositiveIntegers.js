6bd3157bff4822ede8dadeff4cb220ab07a95f0f
hiluan <apt.muenchen@gmail.com>
2022-05-02 01:16:56 -0700

    Sum of two lowest positive integers

---
+++ lvl7/SumOfTwoLowestPositiveIntegers.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
+
+function sumTwoSmallestNumbers(numbers) {
+  //Code here
+  number.sort((a, b) => a - b);
+  return numbers[0] + numbers[1];
+
+  console.log(numbers);
+}
