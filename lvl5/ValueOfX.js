8051c48a184b5508cb8d32a9f7ecc50da32ba718
hiluan <apt.muenchen@gmail.com>
2022-05-07 01:49:39 -0700

    Value of x testing

--- lvl5/ValueOfX.js
+++ lvl5/ValueOfX.js
@@ -3,92 +3,73 @@ function valueOfX(eq) {
   eq = eq.split(" = ");
   eqLeft = eq[0].split(" ");
   eqRight = eq[1].split(" ");
-
   let sumArr = [];
-  let equationCalculator = 1;
 
-  //   for (let i = 0; i < eqLeft.length; i++) {
-  //     eqLeft[i] === "-"
-  //       ? eqLeft[i + 1] === "x"
-  //         ? (equationCalculator = -1)
-  //         : sumArr.push(+eqLeft[i + 1])
-  //       : eqLeft[i + 1] === "x"
-  //       ? (equationCalculator = 1)
-  //       : sumArr.push(-eqLeft[i + 1]);
-  //   }
+  const getNumEq = (eqArr, sumArr) => {
+    let equationCalculator = 1;
+    let tempArr = [];
 
-  //   for (let i = 0; i < eqRight.length; i++) {
-  //     eqRight[i] === "-"
-  //       ? eqRight[i + 1] === "x"
-  //         ? (equationCalculator = -1)
-  //         : sumArr.push(-eqRight[i + 1])
-  //       : eqRight[i + 1] === "x"
-  //       ? (equationCalculator = 1)
-  //       : sumArr.push(eqRight[i + 1]);
-  //   }
-  console.log(eqRight);
-
-  //   eqLeft = eqLeft.map((val, index, eqLeft) => {
-  //     if (val === "-") {
-  //       console.log(val);
-  //       val = "aaa";
-  //     } else {
-  //       //   console.log(val);
-  //       return (eqLeft[index + 1] = -eqLeft[index + 1]);
-  //     }
-  //   });
-
-  //   eqRight = eqRight.map((val, index, eqRight) => {
-  //     console.log(val, index);
-  //     if (val === "-") {
-  //       return (eqRight[index + 1] = -eqRight[index + 1]);
-  //     }
-  //   });
-
-  for (let i = 0; i < eqRight.length; i++) {
-    if (eqRight[i] === "-") {
-      if (eqRight[i + 1] === "x") {
-        equationCalculator = 1;
-      } else {
-        sumArr.push(-eqRight[i + 1]);
-        eqRight.splice(i + 1, 1);
+    for (let i = 0; i < eqArr.length; ++i) {
+      if (eqArr[i] === "x") {
+        if (eqArr[i - 1] === "-") {
+          equationCalculator = 1;
+        } else if (eqArr[i - 1] === "+" || eqArr[i - 1] === undefined) {
+          // undefined meaning there's no - or + before x
+          equationCalculator = -1;
+        }
+        if (eqArr[i + 1] === undefined) {
+          //   console.log(eqArr[i + 1]);
+          tempArr.push(0);
+        }
       }
-    } else if (eqRight[i] === "+") {
-      if (eqRight[i + 1] === "x") {
-        equationCalculator = -1;
-      } else {
-        sumArr.push(eqRight[i + 1]);
-        eqRight.splice(i + 1, 1);
-      }
-    }
-    if (!isNaN(+eqRight[i])) {
-      sumArr.push(+eqRight[i]);
-    }
-  }
 
-  for (let i = 0; i < eqLeft.length; i++) {
-    if (eqLeft[i] === "-") {
-      if (eqLeft[i + 1] === "x") {
-        equationCalculator = -1;
-      } else {
-        sumArr.push(-eqLeft[i + 1]);
-        eqLeft.splice(i + 1, 1);
+      if (eqArr[i] === "-" && eqArr[i + 1] !== "x") {
+        tempArr.push(-eqArr[i + 1]);
+        eqArr.splice(i + 1, 1);
+      } else if (eqArr[i] === "+") {
+        tempArr.push(+eqArr[i + 1]);
+        eqArr.splice(i + 1, 1);
       }
-    } else if (eqLeft[i] === "+") {
-      if (eqLeft[i + 1] === "x") {
-        equationCalculator = 1;
-      } else {
-        sumArr.push(+eqLeft[i + 1]);
-        eqLeft.splice(i + 1, 1);
+
+      if (!isNaN(+eqArr[i])) {
+        tempArr.push(+eqArr[i]);
       }
+
+      //   if (eqArr[i] === "-") {
+      //     if (eqArr[i + 1] === "x") {
+      //       equationCalculator = 1;
+      //     } else {
+      //       tempArr.push(-eqArr[i + 1]);
+      //       eqArr.splice(i + 1, 1);
+      //     }
+      //   } else if (eqArr[i] === "+") {
+      //     if (eqArr[i + 1] === "x") {
+      //       equationCalculator = -1;
+      //     } else {
+      //       tempArr.push(+eqArr[i + 1]);
+      //       eqArr.splice(i + 1, 1);
+      //     }
+      //   }
+      //   if (!isNaN(+eqArr[i])) {
+      //     tempArr.push(+eqArr[i]);
+      //   }
     }
-    if (!isNaN(+eqLeft[i])) {
-      sumArr.push(+eqLeft[i]);
-    }
-  }
+
+    tempArr = tempArr.map((x) => x * equationCalculator);
+    tempArr = tempArr.reduce((p, c) => p + c, 0);
+    console.log(tempArr);
+
+    sumArr.push(tempArr);
+  };
+
+  getNumEq(eqLeft, sumArr);
+  getNumEq(eqRight, sumArr);
+  //   console.log(sumArr);
 
   console.log(sumArr.reduce((p, c) => p + c, 0));
+  return sumArr.reduce((p, c) => p + c, 0);
 }
 
-let eq = "x + 1 = - 9 - 2";
+// let eq = "x + 1 = - 9 - 2";
+let eq = "- x = - 1";
 valueOfX(eq);
