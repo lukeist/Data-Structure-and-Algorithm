edb55c479b5141e76d65b17174698c4be51494ed
hiluan <apt.muenchen@gmail.com>
2022-06-04 16:12:40 -0700

    leetcode 937 reorder data in log files - localeCompare for lexicographically order

--- leetcode-828-CountUniqueCharactersOfAllSubstringsOfAGivenString.js
+++ leetcode-828-CountUniqueCharactersOfAllSubstringsOfAGivenString.js
@@ -23,86 +23,172 @@
 // Input: s = "LEETCODE"
 // Output: 92
 
+// XA(XaXXAX   =>  XA(Xa)XXAX
+//                 XA(XaX)XAX
+//                 XA(XaXX)AX
+// XAX(aXXAX   =>  XAX(a)XXAX
+//                 XAX(aX)XAX
+//                 XAX(aXX)AX
+
 var uniqueLetterString = function (s) {
-  // create substrings from s
-  //// split s to array - loop through s
-
-  let arrSubstr = Array.from(Array(s.length), (_, i) => {
-    const arrTemp = [];
-    for (let j = i + 1; j <= s.length; j++) {
-      arrTemp.push(s.substring(i, j));
-      //   arrSubstrings.push(subString);
-    }
-    return arrTemp;
-  });
-  //   console.log(arrSubstr);
-
-  ///////
-  for (let i = 0; i < s.length; i++) {}
-
-  //   arrSubstr = arrSubstr.map((a, i) => a[i]);
-  //////////////// 2
-  //   const arrSS = Array.from(Array(s.length), (_, i) => {
-  //     for (let j = i + 1; j <= s.length; j++) {
-  //       arrSubstr.push(s.substring(i, j));
-  //       //   arrSubstrings.push(subString);
-  //     }
-  //     return arrSubstr;
-  //   });
-
-  //////////////// 3
-  //   const arrSS = s.split("").map((_, i, arrS) =>
-  //     //   Array.from(
-  //     //     Array(arrS.length - i),
-  //     //     (e, i) => arrS[arrS.length - i - 1]
-  //     //   )
-
-  //     {
-  //       const arrSubstr = [];
-  //       for (let j = i + 1; j <= arrS.length; j++) {
-  //         arrSubstr.push(s.substring(i, j));
-  //         //   arrSubstrings.push(subString);
-  //       }
-  //       return arrSubstr;
-  //     }
-  //   );
-
-  const arrTest = s
-    .split("")
-    .filter((e, i, arr) => e !== arr[i + 1])
-    .join("");
-  //   .split("");
-  console.log(arrTest);
-
-  const arrSubstrings = [];
-  for (let i = 0; i < arrTest.length; i++) {
-    for (let j = i + 1; j <= arrTest.length; j++) {
-      const subString = arrTest.substring(i, j);
-      arrSubstrings.push(subString);
-    }
-  }
-  ///////////////////////
   // remove not unique chars
   const countUniqueChars = (str) => {
     const map = {};
     // put all char into map
-    for (c of str) {
+    for (const c of str) {
       map[c] = (map[c] || 0) + 1; // count chars
     }
-    // console.log(
-    //   str.split("").filter((c) => map[c] === 1)
-    //   //
-    // );
-    // console.log(map);
     return str.split("").filter((c) => map[c] === 1).length;
   };
   ////////////////////////
+  // create substrings from s
+  let arrSubstr = Array.from(Array(s.length), (_, i) => {
+    const arrTemp = [];
+    for (let j = i + 1; j <= s.length; j++) {
+      let sub = s.substring(i, j);
+      arrTemp.push(sub);
+      //   console.log(sub, sub.charAt(j));
+      //   countJ += sub.length - countUniqueChars(sub);
+    }
+    console.log(arrTemp);
+    let countJ = arrTemp.reduce((p, c) => p + countUniqueChars(c), 0);
 
-  // count length of all substrings but not unique
-  const sumAll = arrSubstrings.reduce((p, c) => p + countUniqueChars(c), 0);
-  console.log(arrSubstrings, sumAll);
-  return sumAll;
+    return countJ;
+  });
+  console.log(
+    arrSubstr,
+    arrSubstr.reduce((p, c) => p + c, 0)
+  );
+  return arrSubstr.reduce((p, c) => p + c, 0);
 };
+
+uniqueLetterString("LEETCODE");
+// uniqueLetterString("ABC");
+// uniqueLetterString("ABA");
+
+// [
+//   [1, 2, 1, 2, 3, 4, 5, 5],
+//   [1, 0, 1, 2, 3, 4, 4],
+//   [1, 2, 3, 4, 5, 4],
+//   [1, 2, 3, 4, 5],
+//   [1, 2, 3, 4],
+//   [1, 2, 3],
+//   [1, 2],
+//   [1],
+// ];
+// [
+//   "L",
+//   "LE",
+//   "LEE",
+//   "LEET",
+//   "LEETC",
+//   "LEETCO",
+//   "LEETCOD",
+//   "LEETCODE",
+//   "E",
+//   "EE",
+//   "EET",
+//   "EETC",
+//   "EETCO",
+//   "EETCOD",
+//   "EETCODE",
+//   "E",
+//   "ET",
+//   "ETC",
+//   "ETCO",
+//   "ETCOD",
+//   "ETCODE",
+//   "T",
+//   "TC",
+//   "TCO",
+//   "TCOD",
+//   "TCODE",
+//   "C",
+//   "CO",
+//   "COD",
+//   "CODE",
+//   "O",
+//   "OD",
+//   "ODE",
+//   "D",
+//   "DE",
+//   "E",
+// ];
+// var uniqueLetterString = function (s) {
+//   ///////////////////////
+//   // remove not unique chars
+//   const countUniqueChars = (str) => {
+//     const map = {};
+//     // put all char into map
+//     for (const c of str) {
+//       map[c] = (map[c] || 0) + 1; // count chars
+//     }
+//     // console.log(
+//     //   str.split("").filter((c) => map[c] === 1)
+//     //   //
+//     // );
+//     // console.log(map);
+//     return str.split("").filter((c) => map[c] === 1).length;
+//   };
+//   ////////////////////////
+//   // create substrings from s
+//   //// split s to array - loop through s
+
+//   let arrSubstr = Array.from(Array(s.length), (_, i) => {
+//     const arrTemp = [];
+//     for (let j = i + 1; j <= s.length; j++) {
+//       const a = countUniqueChars(s.substring(i, j));
+//       arrTemp.push(a);
+//       //   arrSubstrings.push(subString);
+//     }
+//     // console.log(arrTemp.reduce((p, c) => p + c, 0));
+//     return arrTemp.reduce((p, c) => p + c, 0);
+//   });
+//   //   console.log(arrSubstr.reduce((p, c) => p + c, 0));
+//   return arrSubstr.reduce((p, c) => p + c, 0);
+
+//   ///////
+
+//   //   arrSubstr = arrSubstr.map((a, i) => a[i]);
+//   //////////////// 2
+//   //   const arrSS = Array.from(Array(s.length), (_, i) => {
+//   //     for (let j = i + 1; j <= s.length; j++) {
+//   //       arrSubstr.push(s.substring(i, j));
+//   //       //   arrSubstrings.push(subString);
+//   //     }
+//   //     return arrSubstr;
+//   //   });
+
+//   //////////////// 3
+//   //   const arrSS = s.split("").map((_, i, arrS) =>
+//   //     //   Array.from(
+//   //     //     Array(arrS.length - i),
+//   //     //     (e, i) => arrS[arrS.length - i - 1]
+//   //     //   )
+
+//   //     {
+//   //       const arrSubstr = [];
+//   //       for (let j = i + 1; j <= arrS.length; j++) {
+//   //         arrSubstr.push(s.substring(i, j));
+//   //         //   arrSubstrings.push(subString);
+//   //       }
+//   //       return arrSubstr;
+//   //     }
+//   //   );
+
+//   //   const arrSubstrings = [];
+//   //   for (let i = 0; i < arrTest.length; i++) {
+//   //     for (let j = i + 1; j <= arrTest.length; j++) {
+//   //       const subString = arrTest.substring(i, j);
+//   //       arrSubstrings.push(subString);
+//   //     }
+//   //   }
+
+//   // count length of all substrings but not unique
+//   //   const sumAll = arrSubstrings.reduce((p, c) => p + countUniqueChars(c), 0);
+//   //   console.log(arrSubstrings, sumAll);
+//   //   return sumAll;
+// };
 
 // // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // // working solutoin but O(n^2) time exceeded
@@ -129,7 +215,7 @@ var uniqueLetterString = function (s) {
 // };
 
 // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-uniqueLetterString("LEETCODE");
+
 // uniqueLetterString(
 //     "DELQGVWNZKIJJPSXOVWWIZUXCEGWSQLESNSRBMKZARFPAXSVWQEZDENDAHNNIBHGHTFDLPGDLFXMIYRFNLMXHNPIFUAXINXPXLCTTJNLGGMKJIOEWBECNOFQPVCIKIAZMNGHEHFMCPWSMJTMGVSXTOGCGUYKFMNCGLCBRAFJLJVPIVDOLJBURULPGXBVDCEWXXXLTRMSHPKSPFDGNVOCZWDXJUWVNAREDOKTZMIUDKDQWWWSAEUUDBHMWZELOSBIHMAYJEMGZPMDOOGSCKLVHTGMETHUISCLJKDOQEWGVBULEMUXGTRKGXYFDIZTZWMLOFTCANBGUARNWQEQWGMIKMORVQUZANJNRNPMJWYLVHWKDFLDDBBMILAKGFROEQAMEVONUVHOHGPKLBPNYZFPLXNBCIFENCGIMIDCXIIQJWPVVCOCJTSKSHVMQJNLHSQTEZQTTMOXUSKBMUJEJDBJQNXECJGSZUDENJCPTTSREKHPRIISXMWBUGMTOVOTRKQCFSDOTEFPSVQINYLHXYVZTVAMWGPNKIDLOPGAMWSKDXEPLPPTKUHEKBQAWEBMORRZHBLOGIYLTPMUVBPGOOOIEBJEGTKQKOUURHSEJCMWMGHXYIAOGKJXFAMRLGTPNSLERNOHSDFSSFASUJTFHBDMGBQOKZRBRAZEQQVWFRNUNHBGKRFNBETEDJIWCTUBJDPFRRVNZENGRANELPHSDJLKVHWXAXUTMPWHUQPLTLYQAATEFXHZARFAUDLIUDEHEGGNIYICVARQNRJJKQSLXKZZTFPVJMOXADCIGKUXCVMLPFJGVXMMBEKQXFNXNUWOHCSZSEZWZHDCXPGLROYPMUOBDFLQMTTERGSSGVGOURDWDSEXONCKWHDUOVDHDESNINELLCTURJHGCJWVIPNSISHRWTFSFNRAHJAJNNXKKEMESDWGIYIQQRLUUADAXOUEYURQRVZBCSHXXFLYWFHDZKPHAGYOCTYGZNPALAUZSTOU"
 //   );
--- leetcode-937-ReorderDataInLogFiles.js
+++ leetcode-937-ReorderDataInLogFiles.js
@@ -15,14 +15,45 @@
 // Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
 
 var reorderLogFiles = function (logs) {
-  //
-  console.log(logs.sort().reverse());
-  //   return logs.sort();
+  // determine if dig or let, push to respective arr
+  //// loop throught logs, use conditionals
+  let arrDig = [];
+  let arrLet = [];
+
+  for (const log of logs) {
+    const firstSpaceIndex = log.indexOf(" ");
+    const indexOfSecondWord = firstSpaceIndex + 1;
+    const firstCharOfSecondWord = log.charAt(indexOfSecondWord);
+    +firstCharOfSecondWord === parseInt(firstCharOfSecondWord)
+      ? arrDig.push(log)
+      : arrLet.push(log);
+  }
+  // sort both arrs
+
+  arrLet.sort((a, b) => {
+    const aContent = a.slice(a.indexOf(" ") + 1);
+    const bContent = b.slice(b.indexOf(" ") + 1);
+    return aContent === bContent
+      ? a.localeCompare(b)
+      : aContent.localeCompare(bContent);
+  });
+
+  // concat both arrs back into 1
+  return arrLet.concat(arrDig);
 };
 reorderLogFiles([
+  "let4 art can",
   "dig1 8 1 5 1",
   "let1 art can",
   "dig2 3 6",
   "let2 own kit dig",
   "let3 art zero",
+]);
+
+reorderLogFiles([
+  "a1 9 2 3 1",
+  "g1 act car",
+  "zo4 4 7",
+  "ab1 off key dog",
+  "a8 act zoo",
 ]);
