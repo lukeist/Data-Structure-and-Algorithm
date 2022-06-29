4570d4fda126282c5a19908c032d084a1e6e8384
hiluan <apt.muenchen@gmail.com>
2022-06-29 03:56:00 +0100

    structy 006 uncompress 2 pointers - use array for better space complexity

--- structy-006-uncompress.js
+++ structy-006-uncompress.js
@@ -3,25 +3,42 @@
 // p: str
 // r: str
 // e: uncompress("2c3a1t"); // -> 'ccaaat'
-
+// two pointers, using array for space complexity bcuz string concat creates another string
 const uncompress = (s) => {
-  let result = "";
+  let result = [];
   let indexOfLastChar = 0;
   for (let i = 0; i < s.length; i++) {
     let char = s[i];
     let times = s.slice(indexOfLastChar, i);
-    console.log(indexOfLastChar, times);
 
     if (char.toLowerCase() !== char.toUpperCase()) {
-      result = result + char.repeat(times);
+      result.push(char.repeat(times));
       indexOfLastChar = i + 1;
     }
   }
 
-  console.log(result);
-  return result;
+  console.log(result.join(""));
+  return result.join("");
 };
 
 // uncompress("2c3a1t");
 
 uncompress("3n12e2z");
+
+// const uncompress = (s) => {
+//   let result = "";
+//   let indexOfLastChar = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     let char = s[i];
+//     let times = s.slice(indexOfLastChar, i);
+//     console.log(indexOfLastChar, times);
+
+//     if (char.toLowerCase() !== char.toUpperCase()) {
+//       result = result + char.repeat(times);
+//       indexOfLastChar = i + 1;
+//     }
+//   }
+
+//   console.log(result);
+//   return result;
+// };
