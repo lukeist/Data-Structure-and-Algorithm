3be7741d8e6d2379b8548ba34da1f35830939055
hiluan <apt.muenchen@gmail.com>
2022-06-30 13:36:00 +0100

    structy 007 compress two pointers + array

--- structy-007-compress.js
+++ structy-007-compress.js
@@ -6,19 +6,39 @@
 
 const compress = (s) => {
   const result = [];
-  let j = 1;
+  let j = 0;
+  let i = 0;
 
-  for (let i = 1; i <= s.length; i++) {
-    if (s[i] !== s[i - 1]) {
-      console.log(result);
-      result.push(j === 1 ? s[i - 1] : j + s[i - 1]);
-      j = 1;
+  while (i <= s.length) {
+    if (s[i] !== s[j]) {
+      //   console.log(s[i - 1], s[i], j, i);
+      let count = i - j;
+      result.push(count === 1 ? s[j] : count + s[j]);
+      j = i;
     } else {
-      j++;
+      i++;
     }
   }
+
   console.log(result);
   return result.join("");
 };
 
 compress("ccaaatsss");
+
+// const compress = (s) => {
+//     const result = [];
+//     let j = 1;
+
+//     for (let i = 1; i <= s.length; i++) {
+//       if (s[i] !== s[i - 1]) {
+//         console.log(result);
+//         result.push(j === 1 ? s[i - 1] : j + s[i - 1]);
+//         j = 1;
+//       } else {
+//         j++;
+//       }
+//     }
+//     console.log(result);
+//     return result.join("");
+//   };
