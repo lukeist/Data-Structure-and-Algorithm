46dc8a897d43c5bc23ac5dc72bd36c66d36d7e9d
hiluan <apt.muenchen@gmail.com>
2022-07-02 12:11:00 +0100

    anagrams hash map time O(n+m) space O(n)

--- structy-008-anagrams.js
+++ structy-008-anagrams.js
@@ -4,21 +4,23 @@
 // r: boolean
 // e: anagrams('cats', 'tocs'); // -> false
 
+// time O(n + m)
+// space O(n)
 const anagrams = (s1, s2) => {
   if (s1.length !== s2.length) return false;
 
-  const mapS1 = {};
-  const mapS2 = {};
+  let count = {};
 
-  for (let i = 0; i < s1.length; i++) {
-    mapS1[s1[i]] = (mapS1[s1[i]] || 0) + 1;
-    mapS2[s2[i]] = (mapS2[s2[i]] || 0) + 1;
+  for (let char of s1) {
+    count[char] = (count[char] || 0) + 1;
   }
 
-  for (let char in mapS1) {
-    if (mapS1[char] !== mapS2[char]) {
-      return false;
-    }
+  for (let char of s2) {
+    count[char]--;
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) return false;
   }
 
   return true;
@@ -26,3 +28,24 @@ const anagrams = (s1, s2) => {
 
 console.log(anagrams("restful", "fluster")); // -> true)
 console.log(anagrams("cats", "tocs")); // -> true)
+// // time O(n + m)
+// // space O(n + m)
+// const anagrams = (s1, s2) => {
+//     if (s1.length !== s2.length) return false;
+
+//     const mapS1 = {};
+//     const mapS2 = {};
+
+//     for (let i = 0; i < s1.length; i++) {
+//       mapS1[s1[i]] = (mapS1[s1[i]] || 0) + 1;
+//       mapS2[s2[i]] = (mapS2[s2[i]] || 0) + 1;
+//     }
+
+//     for (let char in mapS1) {
+//       if (mapS1[char] !== mapS2[char]) {
+//         return false;
+//       }
+//     }
+
+//     return true;
+//   };
