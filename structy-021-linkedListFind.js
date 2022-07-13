c17cb5f446fb3a2dfb1bb7a4b115a6227909cfdd
hiluan <apt.muenchen@gmail.com>
2022-07-13 12:34:00 +0100

    linked list find - bfs + dfs (recursion)

---
+++ structy-021-linkedListFind.js
@@ -0,0 +1,49 @@
+// https://structy.net/problems/linked-list-find
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// p: head ll, num/str
+// r: boolean
+
+// dfs recursion
+const linkedListFind = (head, target) => {
+  if (!head) return false;
+
+  if (head === target) return true;
+
+  if (linkedListFind(head.next, target)) return true;
+
+  return false;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListFind(a, "c")); // true
+
+// // bfs
+// const linkedListFind = (head, target) => {
+//     const queue = [head];
+
+//     while (queue.length > 0) {
+//       let current = queue.shift();
+
+//       if (current.val === target) return true;
+//       current.next && queue.push(current.next);
+//     }
+
+//     return false;
+//   };
--- structy-073-tribonacci.js
+++ structy-073-tribonacci.js
@@ -6,13 +6,30 @@
 
 // f(n) = f(n-1) + f(n-2) + f(n-3)
 // base: 0 0 1
-// dp
-const tribonacci = (n) => {
-  const arr = [0, 0, 1];
-  for (let i = 3; i < n; i++) {
-    arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-  }
-  return arr[n - 1];
+// tribonacci(37); // -> 1132436852
+
+// recursion
+const tribonacci = (n, list = {}) => {
+  if (n === 0 || n === 1) return 0;
+  if (n === 2) return 1;
+
+  !(n in list) &&
+    (list[n] =
+      tribonacci(n - 1, list) +
+      tribonacci(n - 2, list) +
+      tribonacci(n - 3, list));
+
+  return list[n];
 };
 
-console.log(tribonacci(14)); // -> 927
+// // dp
+// const tribonacci = (n) => {
+//   const arr = [0, 0, 1];
+//   for (let i = 3; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+//   }
+//   return arr[n];
+// };
+
+// console.log(tribonacci(14)); // -> 927
+console.log(tribonacci(37)); // -> 1132436852
