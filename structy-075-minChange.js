4c25c596631193c449f609455a98c3f89f4b84af
hiluan <apt.muenchen@gmail.com>
2022-07-18 22:51:00 +0100

    !!! min change - dp - recursion

--- structy-075-minChange.js
+++ structy-075-minChange.js
@@ -5,35 +5,43 @@
 // e: minChange(8, [1, 5, 4, 12]); // -> 2, because 4+4 is the minimum coins possible
 // e: minChange(8, [1, 5, 4, 12]); // -> 2, because 4+4 is the minimum coins possible
 
-// const minChange = (amount, coins, memo = {}) => {
-//   if (amount <= 0) return 0;
-//   if (amount in memo) return memo[amount];
+const minChange = (amount, coins) => {
+  let num = _minChange(amount, coins);
+  return num === Infinity ? -1 : num;
+};
 
-//   for (let coin of coins) {
-//     {
-//       memo[amount] = minChange(amount - coin, coins, memo) + 1;
-//     }
-//     console.log(memo);
-//   }
+const _minChange = (amount, coins, memo = {}) => {
+  if (amount === 0) return 0;
+  if (amount < 0) return Infinity;
 
-//   return memo[amount];
-// };
-
-const minChange = (amount, coins, memo = {}) => {
-  if (amount === 0) return true;
-  if (amount < 0) return false;
   if (amount in memo) return memo[amount];
 
+  let min = Infinity;
+
   for (let coin of coins) {
-    if (minChange(amount - coin, coins, memo)) {
-      memo[amount] = true;
-      return true;
-    }
-    console.log(memo);
+    const numCoin = 1 + _minChange(amount - coin, coins, memo);
+    min = Math.min(min, numCoin);
   }
 
-  memo[amount] = false;
-  return false;
+  memo[amount] = min;
+  return min;
 };
 
 console.log(minChange(8, [1, 5, 4, 12])); // -> 2
+// console.log(minChange(4, [1, 2, 3])); // -> 2
+
+// const minChange = (amount, coins, memo = {}) => {
+//     if (amount === 0) return true;
+//     if (amount < 0) return false;
+//     if (amount in memo) return memo[amount];
+
+//     for (const coin of coins) {
+//       if (minChange(amount - coin, coins, memo)) {
+//         memo[amount] = true;
+//         return true;
+//       }
+//     }
+
+//     memo[amount] = false;
+//     return false;
+//   };
