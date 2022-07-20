dd900548247fd256aaff32ba9da62db4288004b9
hiluan <apt.muenchen@gmail.com>
2022-07-20 15:11:00 +0100

    !!!shortest parth - graph - bfs - why bfs is better than dfs

--- structy-060-shortestPath.js
+++ structy-060-shortestPath.js
@@ -1,35 +1,59 @@
 // p: arr of edges & 2 nodes
 // r: num/-1
 
-// bfs iteration
+// bfs better
 const shortestPath = (edges, nodeA, nodeB) => {
   const graph = makeGraph(edges);
-  const count = {};
-  count[nodeA] = 0;
 
-  const visited = new Set();
-  visited.add(nodeA);
+  const visited = new Set([nodeA]);
 
-  const queue = [nodeA];
+  const queue = [[nodeA, 0]];
 
   while (queue.length > 0) {
-    const current = queue.shift();
+    const [current, distance] = queue.shift();
 
-    if (current === nodeB) return count[current];
+    if (current === nodeB) return distance;
 
     for (const neighbor of graph[current]) {
       if (!visited.has(neighbor)) {
-        count[neighbor] = count[current] + 1;
-        queue.push(neighbor);
+        queue.push([neighbor, distance + 1]);
         visited.add(neighbor);
       }
     }
-
-    console.log(count, visited, queue);
   }
 
   return -1;
 };
+
+// // bfs
+// const shortestPath = (edges, nodeA, nodeB) => {
+//   const graph = makeGraph(edges);
+//   const count = {};
+//   count[nodeA] = 0;
+
+//   const visited = new Set();
+//   visited.add(nodeA);
+
+//   const queue = [nodeA];
+
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+
+//     if (current === nodeB) return count[current];
+
+//     for (const neighbor of graph[current]) {
+//       if (!visited.has(neighbor)) {
+//         count[neighbor] = count[current] + 1;
+//         queue.push(neighbor);
+//         visited.add(neighbor);
+//       }
+//     }
+
+//     console.log(count, visited, queue);
+//   }
+
+//   return -1;
+// };
 
 const makeGraph = (edges) => {
   const graph = {};
