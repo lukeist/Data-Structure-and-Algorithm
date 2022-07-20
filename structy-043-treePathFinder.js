9cdc9e20a1517a0e7ee1f5ed7553a71445509746
hiluan <apt.muenchen@gmail.com>
2022-07-20 15:11:00 +0100

    shortest Path - undirected graph - bfs

---
+++ structy-043-treePathFinder.js
@@ -0,0 +1,105 @@
+// https://structy.net/problems/premium/tree-path-finder
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// p: root of bi tree & val (str/num)
+// r: arr / null
+
+// dfs recusion 2
+const pathFinder = (root, target, arr = []) => {
+  if (!root) return null;
+  if (root.val === target) arr.push(target);
+
+  if (pathFinder(root.right, target, arr)) {
+    arr.push(root.val);
+    root.val = target;
+  }
+  if (pathFinder(root.left, target, arr)) {
+    arr.push(root.val);
+    root.val = target;
+  }
+  //   pathFinder(root.right, target, arr).includes(target) && arr.push(root.val);
+  //   pathFinder(root.left, target, arr).includes(target) && arr.push(root.val);
+
+  return arr;
+};
+
+// // dfs recursion 1
+// const pathFinder = (root, target) => {
+//   const arr = [];
+
+//   explore(root, target, arr);
+
+//   return arr.length === 0 ? null : arr.reverse();
+// };
+
+// const explore = (root, target, arr) => {
+//   if (!root) return null;
+//   if (root.val === target) arr.push(target);
+
+//   if (
+//     explore(root.right, target, arr) === target ||
+//     explore(root.left, target, arr) === target
+//   ) {
+//     arr.push(root.val);
+//     root.val = target;
+//   }
+
+//   return root.val;
+// };
+
+// // dfs iteration NOT WORKING
+// const pathFinder = (root, target) => {
+//   const stack = [root];
+//   const map = {};
+
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+
+//     map[current.val] = [];
+
+//     // if (current.val === "e") return "e";
+
+//     if (current.left) {
+//       map[current.val].push(current.left.val);
+//       stack.push(current.left);
+//     }
+
+//     if (current.right) {
+//       map[current.val].push(current.right.val);
+//       stack.push(current.right);
+//     }
+
+//     // console.log(current);
+//   }
+
+//   return map;
+// };
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(pathFinder(a, "e")); // -> [ 'a', 'b', 'e' ]
+// console.log(pathFinder(a, "p")); // -> null
---
+++ structy-060-shortestPath.js
@@ -0,0 +1,82 @@
+// p: arr of edges & 2 nodes
+// r: num/-1
+
+// bfs iteration
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = makeGraph(edges);
+  const count = {};
+  count[nodeA] = 0;
+
+  const visited = new Set();
+  visited.add(nodeA);
+
+  const queue = [nodeA];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    if (current === nodeB) return count[current];
+
+    for (const neighbor of graph[current]) {
+      if (!visited.has(neighbor)) {
+        count[neighbor] = count[current] + 1;
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    }
+
+    console.log(count, visited, queue);
+  }
+
+  return -1;
+};
+
+const makeGraph = (edges) => {
+  const graph = {};
+  for (const edge of edges) {
+    const [a, b] = edge;
+
+    !(a in graph) && (graph[a] = []);
+    !(b in graph) && (graph[b] = []);
+
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+
+  return graph;
+};
+
+// const edges = [
+//   ["w", "x"],
+//   ["x", "y"],
+//   ["z", "y"],
+//   ["z", "v"],
+//   ["w", "v"],
+// ];
+
+// console.log(shortestPath(edges, "w", "z")); // -> 2
+// console.log(shortestPath(edges, "y", "x")); // -> 1
+
+// const edges = [
+//   ["a", "c"],
+//   ["a", "b"],
+//   ["c", "b"],
+//   ["c", "d"],
+//   ["b", "d"],
+//   ["e", "d"],
+//   ["g", "f"],
+// ];
+
+// console.log(shortestPath(edges, "a", "e")); // -> 3
+
+const edges = [
+  ["a", "c"],
+  ["a", "b"],
+  ["c", "b"],
+  ["c", "d"],
+  ["b", "d"],
+  ["e", "d"],
+  ["g", "f"],
+];
+
+console.log(shortestPath(edges, "b", "g")); // -> -1
