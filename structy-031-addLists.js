ffebe775a932fd36171461819730d7453efd1c4e
hiluan <apt.muenchen@gmail.com>
2022-08-05 15:23:00 +0100

    linked list - add list - iteration + recursion

---
+++ structy-031-addLists.js
@@ -0,0 +1,385 @@
+// https://structy.net/problems/premium/add-lists
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// p: heads of 2 ll
+// r: head of new ll
+
+//          1 -> 4 -> 5 -> 7
+//                   c1
+//          2 -> 3
+//                   c2
+// null ->  3 -> 7 -> 5 -> 7
+//  h            t    c
+
+// iteration not working yet
+const addLists = (head1, head2) => {
+  const head = new Node(null);
+
+  let tail = head;
+
+  let cur1 = head1;
+  let cur2 = head2;
+  let carry = 0;
+
+  // console.log(carry);
+
+  while (cur1 || cur2) {
+    let cur = new Node(null);
+
+    let cur1Val = cur1 ? cur1.val : 0;
+    let cur2Val = cur2 ? cur2.val : 0;
+    let curVal = cur1Val + cur2Val + carry;
+
+    if (curVal >= 10) {
+      cur.val = curVal - 10;
+      carry = 1;
+    } else {
+      cur.val = curVal;
+      carry = 0;
+    }
+
+    tail.next = cur;
+    tail = tail.next;
+
+    cur1.next && (cur1 = cur1.next);
+    cur2.next && (cur2 = cur2.next);
+  }
+
+  carry > 0 && (tail.next = new Node(carry));
+
+  return head.next;
+};
+
+// // recursion BETTER
+// const addLists = (head1, head2, carry = 0) => {
+//   if (!head1 && !head2 && carry === 0) return null;
+
+//   let sum = (head1 ? head1.val : 0) + (head2 ? head2.val : 0) + carry;
+
+//   carry = sum >= 10 ? 1 : 0;
+
+//   const head = new Node(sum % 10);
+
+//   const next1 = !head1 ? null : head1.next;
+//   const next2 = !head2 ? null : head2.next;
+
+//   head.next = addLists(next1, next2, carry)
+
+//   return head;
+// };
+
+// // recursion WORKING GOOD
+// const addLists = (head1, head2, carry = 0) => {
+//   if (!head1 && !head2 && carry === 1) return new Node(1);
+//   if (!head1 && !head2) return null;
+
+//   const head = new Node(null);
+
+//   let sum = (head1 ? head1.val : 0) + (head2 ? head2.val : 0) + carry;
+
+//   if (sum >= 10) {
+//     head.val = sum - 10;
+//     carry = 1;
+//   } else {
+//     head.val = sum;
+//     carry = 0;
+//   }
+
+//   !head1 && (head.next = addLists(null, head2.next, carry));
+//   !head2 && (head.next = addLists(head1.next, null, carry));
+//   head1 && head2 && (head.next = addLists(head1.next, head2.next, carry));
+
+//   return head;
+// };
+
+// //   39
+// // + 47
+// // ----
+// //   86
+
+// // 9 -> 3
+// // 7 -> 4
+// // 6 -> 8
+
+// const a1 = new Node(9);
+// const a2 = new Node(3);
+// a1.next = a2;
+// // 9 -> 3
+
+// const b1 = new Node(7);
+// const b2 = new Node(4);
+// b1.next = b2;
+// // 7 -> 4
+// // 6 -> 8
+
+// //   621
+// // + 354
+// // -----
+// //   975
+
+// const a1 = new Node(1);
+// const a2 = new Node(2);
+// const a3 = new Node(6);
+// a1.next = a2;
+// a2.next = a3;
+// // 1 -> 2 -> 6
+
+// const b1 = new Node(4);
+// const b2 = new Node(5);
+// const b3 = new Node(3);
+// b1.next = b2;
+// b2.next = b3;
+// // 4 -> 5 -> 3
+// // 5 -> 7 -> 9
+
+//  7541
+// +  32
+// -----
+//  7573
+
+// const a1 = new Node(1);
+// const a2 = new Node(4);
+// const a3 = new Node(5);
+// const a4 = new Node(7);
+// a1.next = a2;
+// a2.next = a3;
+// a3.next = a4;
+// // 1 -> 4 -> 5 -> 7
+
+// const b1 = new Node(2);
+// const b2 = new Node(3);
+// b1.next = b2;
+// // 2 -> 3
+// // 3 -> 7 -> 5 -> 7
+
+// const a1 = new Node(9);
+// const a2 = new Node(9);
+// const a3 = new Node(9);
+// a1.next = a2;
+// a2.next = a3;
+// // 9 -> 9 -> 9
+
+// const b1 = new Node(6);
+// // 6
+
+// // 5 -> 0 -> 0 -> 1
+
+// iteration: WORKING BUT CODE is TOO LONG
+// const addLists = (head1, head2) => {
+//   const head = new Node(null);
+
+//   let tail = head;
+
+//   let cur1 = head1;
+//   let cur2 = head2;
+//   let carry = 0;
+
+//   // console.log(carry);
+
+//   while (cur1 && cur2) {
+//     let cur = new Node(null);
+//     let curVal = cur1.val + cur2.val + carry;
+
+//     if (curVal >= 10) {
+//       //
+//       cur.val = curVal - 10;
+//       carry = 1;
+//     } else {
+//       cur.val = curVal;
+//       carry = 0;
+//     }
+
+//     tail.next = cur;
+//     tail = tail.next;
+
+//     cur1 = cur1.next;
+//     cur2 = cur2.next;
+//   }
+
+//   // cur1 && (tail.next = cur1);
+//   // cur2 && (tail.next = cur2);
+
+//   // let [nextHead, nextTail] = findTheRest(tail, cur1 ? cur1 : cur2, carry);
+//   // tail.next = nextHead;
+//   // carry > 0 && (nextTail = new Node(carry));
+
+//   //        a ----> b ----> c
+//   // t t.n  c
+
+//   if (cur1) {
+//     while (cur1) {
+//       let curVal = cur1.val + carry;
+
+//       if (curVal >= 10) {
+//         cur1.val = curVal - 10;
+//         carry = 1;
+//       } else {
+//         cur1.val = curVal;
+//         carry = 0;
+//       }
+
+//       tail.next = cur1;
+//       tail = tail.next;
+//       cur1 = cur1.next;
+//     }
+//   }
+
+//   if (cur2) {
+//     while (cur2) {
+//       let curVal = cur2.val + carry;
+
+//       if (curVal >= 10) {
+//         cur2.val = curVal - 10;
+//         carry = 1;
+//       } else {
+//         cur2.val = curVal;
+//         carry = 0;
+//       }
+
+//       tail.next = cur2;
+//       tail = tail.next;
+//       cur2 = cur2.next;
+//     }
+//   }
+
+//   carry > 0 && (tail.next = new Node(carry));
+
+//   return head.next;
+// };
+
+// const findTheRest = (tail, cur, carry) => {
+//   const headR = cur;
+//   while (cur) {
+//     let curVal = cur.val + carry;
+
+//     if (curVal >= 10) {
+//       cur.val = curVal - 10;
+//       carry = 1;
+//     } else {
+//       cur.val = curVal;
+//       carry = 0;
+//     }
+
+//     tail.next = cur;
+//     tail = tail.next;
+//     cur = cur.next;
+//   }
+//   return [headR, tail];
+// };
+
+// const addLists = (head1, head2) => {
+//   // todo
+//   const head = new Node(null);
+//   let tail = head;
+
+//   let cur1 = head1;
+//   let cur2 = head2;
+
+//   let carry = 0;
+
+//   // together
+//   while (cur1 && cur2) {
+//     let current;
+//     let sumDigit = cur1.val + cur2.val + carry;
+
+//     if (sumDigit > 9) {
+//       sumDigit = sumDigit - 10;
+//       current = new Node(sumDigit);
+//       carry = 1;
+//     } else {
+//       current = new Node(sumDigit);
+//       carry = 0;
+//     }
+
+//     tail.next = current;
+//     tail = current;
+
+//     cur1 = cur1.next;
+//     cur2 = cur2.next;
+//   }
+
+//   // separate: rest of 1 tail
+
+//   //   while (cur1) {
+//   //     tail.next = cur1;
+//   //     tail = tail.next;
+
+//   //     cur1 = cur1.next;
+//   //   }
+
+//   //   while (cur1) {
+//   //     tail.next = cur2;
+//   //     tail = tail.next;
+
+//   //     cur2 = cur2.next;
+//   //   }
+
+//   //   cur1 && (tail.next = tailVal(tail, cur1, carry));
+//   //   cur2 && (tail.next = tailVal(tail, cur1, carry));
+
+//   while (cur1) {
+//     if (cur1.val + carry > 9) {
+//       cur1.val = cur1.val - 10;
+//       cur1.val = cur1.val + carry;
+//       carry = 1;
+//     } else {
+//       cur1.val = cur1.val + carry;
+//       carry = 0;
+//     }
+//     tail.next = cur1;
+//     tail = tail.next;
+//     cur1 = cur1.next;
+//   }
+
+//   while (cur2) {
+//     if (cur2.val + carry > 9) {
+//       cur2.val = cur2.val - 10;
+//       cur2.val = cur2.val + carry;
+//       carry = 1;
+//     } else {
+//       cur2.val = cur2.val + carry;
+//       carry = 0;
+//     }
+//     tail.next = cur2;
+//     tail = tail.next;
+//     cur2 = cur2.next;
+//   }
+
+//   //   tail = tail.next;
+//   //   carry === 1 && (tail.next = new Node(1));
+
+//   return head.next;
+// };
+
+// 6 -> null
+//      c1
+// 5 -> 0 -> 0 -> 1
+// t    c2
+//   999
+//  +  6
+//  ----
+//  1005
+
+// const tailVal = (tail, node, carry) => {
+//   while (node) {
+//     if (node.val + carry > 9) {
+//       node.val = node.val - 10;
+//       node.val = node.val + carry;
+//       carry = 1;
+//     } else {
+//       node.val = node.val + carry;
+//       carry = 0;
+//     }
+//     tail.next = node;
+//     tail = tail.next;
+//     node = node.next;
+//   }
+// };
+
+console.log(addLists(a1, b1));
--- structy-082-maxPalinSubsequence.js
+++ structy-082-maxPalinSubsequence.js
@@ -1,30 +1,83 @@
 // https://structy.net/problems/max-palin-subsequence
 
+// alvin's explanation: abdef > abde + bdef
+// why not: abde abdf abef abef adef bdef??????
+
 // p: str
 // r: num+
-const maxPalinSubsequence = (str) => {
-  // todo
-  let max = -Infinity;
+const maxPalinSubsequence = (str, memo = {}) => {
+  if (str.length === 0) return 0;
+  if (str.length === 1) return 1;
 
-  //   for (let i = 0; i < str.length; i++) {
-  //     for (let j = i + 1; j < str.length; j++) {
-  //       const newStr = str.slice(0, i) + str.slice(j);
-  //       //   console.log(newStr, isPalindrome(newStr));
-  //       max = Math.max(isPalindrome(newStr), max);
-  //     }
+  //   if (str.length === 2 && str[0] === str[1]) {
+  //     return 2;
+  //   } else if (str.length === 2) {
+  //     return -Infinity;
   //   }
 
+  // a b c d e f g
+  // 0 1 2 3 4 5 6
+  //   i       7-i-1
+  if (str.length in memo) return memo[str];
+  let max = -Infinity;
+  let count = 0;
+
+  for (
+    let i = 0, j = str.length - 1;
+    i < str.length / 2, j > str.length / 2;
+    i++, j--
+  ) {
+    if (str[i] === str[j]) {
+      str = str.slice(0, i) + str.slice(i + 1, j) + str.slice(j + 1);
+
+      count = (count + 1) * 2;
+
+      i = 0;
+      j = str.length - 1;
+    }
+  }
+
+  max = count + Math.max(max, isPalindrome(str));
+
+  for (let i = 0; i < str.length; i++) {
+    const newStr = str.slice(0, i) + str.slice(i + 1);
+    max = Math.max(max, maxPalinSubsequence(newStr, memo));
+  }
+
+  memo[str] = max;
   return max;
 };
 
 const isPalindrome = (str) => {
-  //   console.log(str, str.split("").reverse().join(""));
+  // option 1
+  //   const checkPali = str === str.split("").reverse().join("");
 
-  const checkPali = str === str.split("").reverse().join("");
+  // option 2
+  let strReversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    strReversed = strReversed + str[i];
+  }
+  const checkPali = str === strReversed;
+
+  // return
   if (checkPali) return str.length;
-
   return -Infinity;
 };
 
 console.log(maxPalinSubsequence("luwxult")); // -> 5
+console.log(maxPalinSubsequence("xyzaxxzy")); // -> 6
+console.log(maxPalinSubsequence("lol")); // -> 3
+console.log(maxPalinSubsequence("boabcdefop")); // -> 3
+// console.log(maxPalinSubsequence("chartreusepugvicefree")); // -> 7
+
 //   luwul
+// case 1: 6
+// case 2: 6x5
+// case 3: 4
+// case 4: 3
+// case
+
+//                                luwxult
+//           /       /     /      /       \         \        \
+//     uwxult  lwxult luxult  luwult      luwxlt   luwxut    luwxul
+//     ///\\\  ///\\\ ///\\\  ///\\\      ///\\\   ///\\\    ///\\\
