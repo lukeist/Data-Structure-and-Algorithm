ef657a32c500f9ae0cc4306fac0bb9c27ece5aab
hiluan <apt.muenchen@gmail.com>
2022-08-06 05:33:00 +0100

    bi tree - leaf list - dfs only (iter. & recur.)

---
+++ structy-050-leafList.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// p: root of bi tree
+// r: arr
+
+// dfs recursion
+const leafList = (root) => {
+  if (!root) return [];
+  if (!root.left && !root.right) leaves.push(root.val);
+
+  const left = leafList(root.left);
+  const right = leafList(root.right);
+
+  return leaves;
+};
+
+// // dfs iteration
+// const leafList = (root) => {
+//   if (!root) return [];
+
+//   const stack = [[root]];
+//   const leaves = [];
+
+//   while (stack.length > 0) {
+//     const [current] = stack.pop();
+
+//     !current.left && !current.right && leaves.push(current);
+
+//     current.left && stack.push([current.left]);
+//     current.right && stack.push([current.right]);
+//   }
+
+//   return leaves;
+// };
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+leafList(a); // -> [ 'd', 'e', 'f' ]
