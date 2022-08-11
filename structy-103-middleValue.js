5a535100203a815978f51ba663d60b6fe644829a
hiluan <apt.muenchen@gmail.com>
2022-08-10 19:17:19 -0700

    linked list - middle value - 2 pointers

--- structy-103-middleValue.js
+++ structy-103-middleValue.js
@@ -10,17 +10,40 @@ class Node {
   }
 }
 
+// a ----> b ----> c ----> d ----> e ----> null
+//                 c1
+//                                c2
+
+// a ----> b ----> c ----> d ----> e ----> f ----> null
+//                 c1
+//                                c2
+
+// two pointers: time O(n) space O(1)
 const middleValue = (head) => {
-  let cur = head;
-  const vals = [];
-  while (cur) {
-    vals.push(cur.val);
-    cur = cur.next;
+  let c1 = head;
+  let c2 = head;
+
+  while (c2) {
+    if (!c2.next) return c1.val;
+
+    c1 = c1.next;
+    c2 = c2.next.next;
   }
 
-  const len = vals.length;
-  return len % 2 === 0 ? vals[len / 2] : vals[len / 2 - 0.5];
+  return c1.next.val;
 };
+// time O(n) space O(n)
+// const middleValue = (head) => {
+//   let cur = head;
+//   const vals = [];
+//   while (cur) {
+//     vals.push(cur.val);
+//     cur = cur.next;
+//   }
+
+//   const len = vals.length;
+//   return len % 2 === 0 ? vals[len / 2] : vals[len / 2 - 0.5];
+// };
 
 const a = new Node("a");
 const b = new Node("b");
