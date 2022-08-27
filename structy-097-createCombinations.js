0b78cd8fc4dc6df52be902432a45df3213826764
hiluan <apt.muenchen@gmail.com>
2022-08-27 21:33:00 +0100

    combination recursion - O(n choose k)

---
+++ structy-097-createCombinations.js
@@ -0,0 +1,29 @@
+// https://structy.net/problems/premium/create-combinations
+
+// p: arr, num
+// r: 2d arr
+
+const createCombinations = (items, k) => {
+  if (k === 0) return [[]];
+  if (k > items.length) return [];
+
+  const e = items[0];
+  const combiWOe = createCombinations(items.slice(1), k);
+
+  const combiWe = createCombinations(items.slice(1), k - 1);
+  combiWe.map((c) => c.push(e));
+
+  //   for (let c of cWOeLeft) {
+  //     c.push(e);
+  //   }
+  //   return cWOeLeft.concat(combiWOe);
+
+  return [...combiWOe, ...combiWe];
+};
+
+console.log(createCombinations(["a", "b", "c"], 2)); // ->
+// [
+//   [ 'a', 'b' ],
+//   [ 'a', 'c' ],
+//   [ 'b', 'c' ]
+// ]
