d7f9bba07001e68e00358922b454fc24a23f33b0
hiluan <apt.muenchen@gmail.com>
2022-09-24 20:38:33 -0700

    dp with memo - recursion O(a x c) O(a)

--- structy-075-minChange-2.js
+++ structy-075-minChange-2.js
@@ -8,32 +8,45 @@
 //         (1 9 5 14 30)
 
 const minChange = (amount, coins) => {
-  let min = Infinity;
+  let min = findMin(amount, coins);
 
-  const explore = (amount, coins, count = 0) => {
-    if (amount < 0) return false;
-    if (amount === 0) return true;
-
-    // count++;
-    for (let coin of coins) {
-      if (explore(amount - coin, coins, count + 1)) {
-        min = Math.min(min, count);
-        console.log(true, min);
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  if (explore(amount, coins, min)) return min;
-
-  return -1;
+  return min === Infinity ? -1 : min;
 };
 
+// recursion W memo O(coins.length x amount) O(coins.length x amount)
+const findMin = (amount, coins, memo = {}) => {
+  if (amount < 0) return Infinity;
+  if (amount === 0) return 0;
+
+  if (amount in memo) return memo[amount];
+
+  let min = Infinity;
+
+  for (let coin of coins) {
+    min = Math.min(min, 1 + findMin(amount - coin, coins, memo));
+  }
+
+  memo[amount] = min;
+  return min;
+};
+
+// // recursion WO memo O(coins.length^amount) O(coins.length^amount)
+// const findMin = (amount, coins) => {
+//   if (amount < 0) return Infinity;
+//   if (amount === 0) return 0;
+
+//   let min = Infinity;
+
+//   for (let coin of coins) {
+//     min = Math.min(min, 1 + findMin(amount - coin, coins));
+//   }
+
+//   return min;
+// }
+
 // console.log(minChange(3, [4, 2, 10])); // -> -1
-console.log(minChange(8, [1, 5, 4, 12])); // -> 2
-// console.log(minChange(13, [1, 9, 5, 14, 30]));
+// console.log(minChange(8, [1, 5, 4, 12])); // -> 2
+console.log(minChange(13, [1, 9, 5, 14, 30]));
 // -> 5
 
 //   module.exports = {
