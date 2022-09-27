8601d4c1b658147144040228a7df7e4b71de7abd
hiluan <apt.muenchen@gmail.com>
2022-09-26 17:36:14 -0700

    bi tree - dfs recursion O(nodes) O(nodes)

---
+++ structy-043-treePathFinder-2.js
@@ -0,0 +1,49 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// p: root of bi tree, val (str)
+// r: arr
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+// recursive dfs O(nodes) O(nodes):
+const pathFinder = (root, target) => {
+  if (!root) return null;
+  if (root.val === target) return [target];
+
+  const left = pathFinder(root.left, target);
+  const right = pathFinder(root.right, target);
+
+  // if (left && left.includes(target)) {
+  if (left) {
+    left.unshift(root.val);
+    return left;
+  }
+
+  // if (right && right.includes(target)) {
+  if (right) {
+    right.unshift(root.val);
+    return right;
+  }
+
+  // if (!left && !right) return null;
+  return null;
+};
+
+// // iterative dfs:
+// const pathFinder = (root, target) => {
+//   // traverse thru each node, add them to arrays of array
+//   // loop thru array & see if any arr.includes('val') => return that arr || null
+// };
+
+module.exports = {
+  pathFinder,
+};
