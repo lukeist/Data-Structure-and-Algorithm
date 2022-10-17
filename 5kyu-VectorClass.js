2c4933908d77fd0b1651eb8f07e87cf2fe568041
hiluan <apt.muenchen@gmail.com>
2022-10-16 22:20:45 -0700

    class testing

--- 5kyu-VectorClass.js
+++ 5kyu-VectorClass.js
@@ -62,3 +62,43 @@ var c = new Vector([5, 6, 7, 8]);
 console.log(a.equals(new Vector([1, 2, 3])));
 console.log(b.equals(new Vector([3, 4, 5])));
 console.log(a.equals(b));
+
+// // better
+// var Vector = function (components) {
+//   this.items = components;
+//   this.length = components.length;
+// };
+
+// Vector.prototype = {
+//   do: function (action, vector) {
+//     if (vector.length !== this.length) {
+//       throw "Different Length!";
+//     }
+//     return new Vector(
+//       this.items.map(function (v, k) {
+//         return eval(v + action + vector.items[k]);
+//       })
+//     );
+//   },
+//   add: function (v) {
+//     return this.do("+", v);
+//   },
+//   subtract: function (v) {
+//     return this.do("-", v);
+//   },
+//   sum: function (v) {
+//     return eval(v.items.join("+"));
+//   },
+//   dot: function (v) {
+//     return this.sum(this.do("*", v));
+//   },
+//   norm: function () {
+//     return Math.sqrt(this.dot(this));
+//   },
+//   toString: function () {
+//     return "(" + this.items + ")";
+//   },
+//   equals: function (v) {
+//     return this.toString() == v.toString();
+//   },
+// };
