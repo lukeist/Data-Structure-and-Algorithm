d903943afc0c0bd2345139341e69ff1650a1bdc9
hiluan <apt.muenchen@gmail.com>
2022-10-25 13:56:44 -0700

    bi tree: r[l]===undefined&&(r[l]=root.val): dfs recu + bfs iter O(n)O(n)

--- structy-107-leftyNodes-2.js
+++ structy-107-leftyNodes-2.js
@@ -9,37 +9,62 @@
 // p: root of bi tree
 // r: arr
 
-// // recursive
-// const leftyNodes = (root, isLeft = true) => {
-//   if (!root) return [];
-
-//   const left = leftyNodes(root.left, true);
-//   const right = leftyNodes(root.right, false);
-
-//   return [isLeft && root.val, ...left];
-// };
-
-// iterative O(nodes) O(nodes)
-const leftyNodes = (root) => {
+// recursive
+const leftyNodes = (root, level = 0, result = []) => {
   if (!root) return [];
-  const queue = [[root, 0]];
-  const map = {};
-  let lastLvl = 0;
 
-  while (queue.length > 0) {
-    const [current, level] = queue.shift();
-    lastLvl = level;
-    map[level] === undefined && (map[level] = []);
-    map[level].push(current.val);
+  result[level] === undefined && (result[level] = root.val);
 
-    current.left && queue.push([current.left, level + 1]);
-    current.right && queue.push([current.right, level + 1]);
-  }
-
-  const result = [];
-  for (let level = 0; level <= lastLvl; level++) {
-    result.push(map[level][0]);
-  }
+  leftyNodes(root.left, level + 1, result);
+  leftyNodes(root.right, level + 1, result);
 
   return result;
+};
+
+// const explore
+
+// // iterative O(nodes) O(nodes)
+// const leftyNodes = (root) => {
+//   if (!root) return [];
+//   const queue = [[root, 0]]
+//   const result = [];
+
+//   while (queue.length > 0) {
+//     const [current, level] = queue.shift();
+//     result[level] === undefined && (result[level] = current.val);
+
+//     current.left && queue.push([current.left, level + 1])
+//     current.right && queue.push([current.right, level + 1])
+//   }
+
+//   return result;
+// };
+
+// // iterative O(nodes) O(nodes)
+// const leftyNodes = (root) => {
+//   if (!root) return [];
+//   const queue = [[root, 0]]
+//   const map = {};
+//   let lastLvl = 0;
+
+//   while (queue.length > 0) {
+//     const [current, level] = queue.shift();
+//     lastLvl = level;
+//     map[level] === undefined && (map[level] = []);
+//     map[level].push(current.val);
+
+//     current.left && queue.push([current.left, level + 1])
+//     current.right && queue.push([current.right, level + 1])
+//   }
+
+//   const result = [];
+//   for (let level = 0; level <= lastLvl; level++) {
+//     result.push(map[level][0]);
+//   }
+
+//   return result;
+// };
+
+module.exports = {
+  leftyNodes,
 };
