7d44cbd8724974f7d1257870fa2716b9ae1abfe7
hiluan <apt.muenchen@gmail.com>
2022-11-02 13:25:09 -0700

    binary search tree - iter O(logn) O(logn) + recu O(logn) O(logn)

---
+++ structy-117-binarySearchTreeIncludes-2.js
@@ -0,0 +1,43 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// p: root of bi search tree, num
+// r: boolean
+
+// recursion O(logn) O(1)
+const binarySearchTreeIncludes = (root, target) => {
+  if (!root) return false;
+  if (root.val === target) return true;
+
+  //   let left, right;
+  //   target < root.val && (left = binarySearchTreeIncludes(root.left, target));
+  //   root.val <= target && (right = binarySearchTreeIncludes(root.right, target));
+
+  //   if (left || right) return true;
+  //   return false;
+
+  if (target < root.val) {
+    return binarySearchTreeIncludes(root.left, target);
+  } else return binarySearchTreeIncludes(root.right, target);
+};
+
+// // iteration O(logn) O(n)
+// const binarySearchTreeIncludes = (root, target) => {
+//   const stack = [root];
+
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+
+//     if (current.val === target) return true
+
+//     current.left && target < current.val && stack.push(current.left);
+//     current.right && current.val <= target && stack.push(current.right);
+//   }
+
+//   return false;
+// };
