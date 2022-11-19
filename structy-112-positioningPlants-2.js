b91e407d043fbb027223a0b94f2afcde903f1044
hiluan <apt.muenchen@gmail.com>
2022-11-19 14:34:52 -0800

    dp grid recu O(r x c) O(rc) - iter O(r x c x c) O(1)

--- structy-112-positioningPlants-2.js
+++ structy-112-positioningPlants-2.js
@@ -55,36 +55,83 @@
 //   return min;
 // };
 
-// dp iteration
-const positioningPlants = (costs) => {
-  for (let i = 1; i < costs.length; i++) {
-    for (let j = 0; j < costs[0].length; j++) {
-      const lastPlant = j;
-      const min = Infinity;
-
-      for (let k = 0; k < costs[0].length; k++) {
-        if (k === lastPlant) continue;
-        min = Math.min(min, costs[i][k]);
-      }
-
-      costs[i][j] += min;
-    }
-  }
-
-  return Math.min(...costs[costs.length - 1]);
-};
-
-// // dp iteration O(n^m)
+// // dp iteration
 // const positioningPlants = (costs) => {
 //   for (let i = 1; i < costs.length; i++) {
 //     for (let j = 0; j < costs[0].length; j++) {
-//       const lastRow = [...costs[i - 1]];
-//       lastRow.splice(j, 1);
-//       costs[i][j] += Math.min(...lastRow);
+//       const lastPlant = j;
+//       const min = Infinity;
+
+//       for (let k = 0; k < costs[0].length; k++) {
+//         if (k === lastPlant) continue;
+//         min = Math.min(min, costs[i][k]);
+//       }
+
+//       costs[i][j] += min;
 //     }
 //   }
 
 //   return Math.min(...costs[costs.length - 1]);
+// };
+
+// // // dp iteration O(n^m)
+
+// const positioningPlants = (grid) => {
+//   for (let i = 1; i < grid.length; i++) {
+//     for (let j = 0; j < grid[0].length; j++) {
+//       let min = Infinity;
+//       for (let k = 0; k < grid[0].length; k++) {
+//         if (k === j) continue;
+//         min = Math.min(min, grid[i - 1][k]);
+//       }
+
+//       grid[i][j] += min;
+//     }
+//   }
+
+//   return Math.min(...grid[grid.length - 1]);
+// };
+
+//                          curr,last
+//                            0,null
+//                  4/            3\               7\
+//                 1,0   .         1,1              1,2
+//              /1   \9         /6     \9           /6   \1
+//          2,1       2,2       2,0     2,2           2,0   2,1
+//       2/   3\     /2   5\
+//     3,0    3,2    3,0   3,1
+// w MEMO
+const positioningPlants = (grid, curr = 0, last = null, memo = {}) => {
+  const currlast = curr + "," + last;
+  if (currlast in memo) return memo[currlast];
+  if (curr === grid.length) return 0;
+
+  let min = Infinity;
+
+  for (let i = 0; i < grid[0].length; i++) {
+    if (i === last) continue;
+    min = Math.min(
+      min,
+      grid[curr][i] + positioningPlants(grid, curr + 1, i, memo)
+    );
+  }
+
+  memo[currlast] = min;
+  return min;
+};
+
+// // no MEMO
+// const positioningPlants = (grid, curr = 0, last = null) => {
+//   if (curr === grid.length) return 0;
+
+//   let min = Infinity;
+
+//   for (let i = 0; i < grid[0].length; i++) {
+//     if (i === last) continue;
+//     min = Math.min(min, grid[curr][i] + positioningPlants(grid, curr + 1, i));
+//   }
+
+//   return min;
 // };
 
 console.log(
