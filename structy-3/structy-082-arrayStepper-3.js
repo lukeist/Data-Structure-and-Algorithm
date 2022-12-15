7597c4dfd8221883b30d2813736f02709eead0f1
hiluan <apt.muenchen@gmail.com>
2022-12-14 16:05:46 -0800

    dp recu memo O(n^2) O(n)

--- structy-3/structy-082-arrayStepper-3.js
+++ structy-3/structy-082-arrayStepper-3.js
@@ -7,6 +7,7 @@
 //              1/ 2\  3\   4\            1/  2\
 //           2001   001   01    1         001   01
 
+// time O(n^2) because nested loops: [7,6,5,4,3,2,1,0,0] worst case
 const arrayStepper = (nums, i = 0, memo = {}) => {
   if (i in memo) return memo[i];
   if (i >= nums.length - 1) return true;
