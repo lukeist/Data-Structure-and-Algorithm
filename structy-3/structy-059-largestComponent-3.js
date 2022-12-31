5e16818dfb86b53e64375394ce4436514b27af2d
hiluan <apt.muenchen@gmail.com>
2022-12-31 12:34:00 +0100

    graph - dfs recu O(edges) O(nodes)

--- structy-3/structy-059-largestComponent-3.js
+++ structy-3/structy-059-largestComponent-3.js
@@ -1,53 +1,86 @@
-// p: obj;
-// r: num;
-//
-//
-// 8
-// | \
-// 0 - 5
-// |
-// 1
-//              4 - 3
-//               \  |
-//                2
-
 const largestComponent = (graph) => {
   const visited = new Set();
-  let max = -Infinity;
-  for (let node in graph) {
-    max = Math.max(max, explore(graph, node, visited));
+  let max = 0;
+  for (let key in graph) {
+    max = Math.max(max, explore(graph, key, visited));
   }
-  return max === -Infinity ? 0 : max;
+
+  return max;
 };
 
 const explore = (graph, node, visited) => {
   if (visited.has(node)) return 0;
   visited.add(node);
+
   let count = 0;
-  const stack = [node];
-
-  while (stack.length > 0) {
-    const c = stack.pop();
-    count++;
-
-    for (let nei of graph[c]) {
-      !visited.has(nei) && visited.add(nei) && stack.push(nei);
-    }
+  for (let nei of graph[node]) {
+    count += explore(graph, nei, visited);
   }
-
-  return count;
+  return count + 1;
 };
+
+console.log(
+  largestComponent({
+    0: ["8", "1", "5"],
+    1: ["0"],
+    5: ["0", "8"],
+    8: ["0", "5"],
+    2: ["3", "4"],
+    3: ["2", "4"],
+    4: ["3", "2"],
+  })
+); // -> 4)
+
+// // p: obj;
+// // r: num;
+// //
+// //
+// // 8
+// // | \
+// // 0 - 5
+// // |
+// // 1
+// //              4 - 3
+// //               \  |
+// //                2
+
+// const largestComponent = (graph) => {
+//   const visited = new Set();
+//   let max = -Infinity;
+//   for (let node in graph) {
+//     max = Math.max(max, explore(graph, node, visited));
+//   }
+//   return max === -Infinity ? 0 : max;
+// };
 
 // const explore = (graph, node, visited) => {
 //   if (visited.has(node)) return 0;
 //   visited.add(node);
-//   let count = 1;
-//   for (let nei of graph[node]) {
-//     count += explore(graph, nei, visited);
-//   }
-//   return count;
-// }
+//   let count = 0;
+//   const stack = [node];
 
-module.exports = {
-  largestComponent,
-};
+//   while (stack.length > 0) {
+//     const c = stack.pop();
+//     count++;
+
+//     for (let nei of graph[c]) {
+//       !visited.has(nei) && visited.add(nei) && stack.push(nei);
+//     }
+//   }
+
+//   return count;
+// };
+
+// // const explore = (graph, node, visited) => {
+// //   if (visited.has(node)) return 0;
+// //   visited.add(node);
+// //   let count = 1;
+// //   for (let nei of graph[node]) {
+// //     count += explore(graph, nei, visited);
+// //   }
+// //   return count;
+// // }
+
+// module.exports = {
+//   largestComponent,
+// };
