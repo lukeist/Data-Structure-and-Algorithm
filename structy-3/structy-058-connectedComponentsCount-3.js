be93c6a3896c1c12c20881761e2bf4da9a237f9b
hiluan <apt.muenchen@gmail.com>
2022-12-30 18:17:48 -0800

    graph - dfs O(edges) O(nodes)

--- structy-3/structy-058-connectedComponentsCount-3.js
+++ structy-3/structy-058-connectedComponentsCount-3.js
@@ -4,20 +4,21 @@ const connectedComponentsCount = (graph) => {
 
   for (let node in graph) {
     if (visited.has(node)) continue;
-    visited.add(node);
-    count++;
+    explore(graph, node, visited);
     console.log(visited);
-    const stack = [node];
-    while (stack.length > 0) {
-      const c = stack.pop();
-      for (let nei of graph[c]) {
-        !visited.has(nei.toString()) &&
-          visited.add(nei.toString()) &&
-          stack.push(nei.toString());
-      }
-    }
+    count++;
   }
+
   return count;
+};
+
+const explore = (graph, node, visited) => {
+  if (visited.has(node)) return;
+  visited.add(node);
+
+  for (let nei of graph[node]) {
+    explore(graph, nei.toString(), visited);
+  }
 };
 
 console.log(
@@ -30,4 +31,38 @@ console.log(
     3: [2, 4],
     4: [3, 2],
   })
-); // -> 2
+); // -> 2)
+
+// const connectedComponentsCount = (graph) => {
+//   const visited = new Set();
+//   let count = 0;
+
+//   for (let node in graph) {
+//     if (visited.has(node)) continue;
+//     visited.add(node);
+//     count++;
+//     console.log(visited);
+//     const stack = [node];
+//     while (stack.length > 0) {
+//       const c = stack.pop();
+//       for (let nei of graph[c]) {
+//         !visited.has(nei.toString()) &&
+//           visited.add(nei.toString()) &&
+//           stack.push(nei.toString());
+//       }
+//     }
+//   }
+//   return count;
+// };
+
+// console.log(
+//   connectedComponentsCount({
+//     0: [8, 1, 5],
+//     1: [0],
+//     5: [0, 8],
+//     8: [0, 5],
+//     2: [3, 4],
+//     3: [2, 4],
+//     4: [3, 2],
+//   })
+// ); // -> 2
