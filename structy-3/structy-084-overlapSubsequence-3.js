c000480f1d3e75acf477a3805932656f5036c563
hiluan <apt.muenchen@gmail.com>
2023-01-01 22:27:23 -0800

    dp - recu with MEMO O(s1.len*s2.len) O(s1.len*s2.len)

--- structy-3/structy-084-overlapSubsequence-3.js
+++ structy-3/structy-084-overlapSubsequence-3.js
@@ -18,21 +18,38 @@
 //                                        x     s
 //                                        t     x
 
-const overlapSubsequence = (str1, str2, i = 0, j = 0) => {
-  if (i === str1.length || j === str2.length) return 0;
-
+const overlapSubsequence = (s1, s2, i = 0, j = 0, memo = {}) => {
+  if (i === s1.length || j === s2.length) return 0;
+  const ij = i + "," + j;
+  if (ij in memo) return memo[ij];
   let count = 0;
-  if (str1[i] === str2[j]) {
-    count += 1 + overlapSubsequence(str1, str2, i + 1, j + 1);
-    console.log(count);
+  if (s1[i] === s2[j]) {
+    count = 1 + overlapSubsequence(s1, s2, i + 1, j + 1, memo);
   } else {
-    count += Math.max(
-      overlapSubsequence(str1, str2, i + 1, j),
-      overlapSubsequence(str1, str2, i, j + 1)
+    count = Math.max(
+      overlapSubsequence(s1, s2, i + 1, j, memo),
+      overlapSubsequence(s1, s2, i, j + 1, memo)
     );
   }
-
+  memo[ij] = count;
   return count;
 };
+
+// const overlapSubsequence = (str1, str2, i = 0, j = 0) => {
+//   if (i === str1.length || j === str2.length) return 0;
+
+//   let count = 0;
+//   if (str1[i] === str2[j]) {
+//     count += 1 + overlapSubsequence(str1, str2, i + 1, j + 1);
+//     console.log(count);
+//   } else {
+//     count += Math.max(
+//       overlapSubsequence(str1, str2, i + 1, j),
+//       overlapSubsequence(str1, str2, i, j + 1)
+//     );
+//   }
+
+//   return count;
+// };
 
 console.log(overlapSubsequence("dogs", "daogt")); // -> 3
