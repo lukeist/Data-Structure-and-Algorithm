9fbcfc051fdeed406c2134e6c18c46e0698b5432
hiluan <apt.muenchen@gmail.com>
2023-01-17 17:36:34 -0800

    exhaustive recursive - O(s.len^max.brackets.len) O(s.len^max.brackets.len)

--- structy-3/structy-100-parentheticalPossibilities-3.js
+++ structy-3/structy-100-parentheticalPossibilities-3.js
@@ -1,3 +1,26 @@
+const parentheticalPossibilities = (s) => {
+  if (s.length === 0) return [""];
+  const [firsts, remaining] = pp(s);
+  const remain = parentheticalPossibilities(remaining);
+  const result = [];
+  for (let r of remain) {
+    for (let f of firsts) {
+      result.push(f + r);
+    }
+  }
+
+  return result;
+};
+
+const pp = (s) => {
+  if (s[0] === "(") {
+    const iClose = s.indexOf(")");
+    return [s.slice(1, iClose).split(""), s.slice(iClose + 1)];
+  } else {
+    return [[s[0]], s.slice(1)];
+  }
+};
+
 // p: str
 // r: arr
 //      x(mn)yz                 ''
@@ -50,31 +73,31 @@
 //   return result;
 // };
 
-const parentheticalPossibilities = (s) => {
-  if (!s) return [""];
-  const { remaining, chars } = getOptions(s);
-  const result = [];
-  const sWOf = parentheticalPossibilities(remaining);
-  for (let str of sWOf) {
-    for (let c of chars) {
-      result.push(c + str);
-    }
-  }
-  return result;
-};
+// const parentheticalPossibilities = (s) => {
+//   if (!s) return [""];
+//   const { remaining, chars } = getOptions(s);
+//   const result = [];
+//   const sWOf = parentheticalPossibilities(remaining);
+//   for (let str of sWOf) {
+//     for (let c of chars) {
+//       result.push(c + str);
+//     }
+//   }
+//   return result;
+// };
 
-const getOptions = (s) => {
-  if (s[0] === "(") {
-    const i = s.indexOf(")");
-    const remaining = s.slice(i + 1);
-    const chars = s.slice(1, i).split("");
-    return { remaining, chars };
-  } else {
-    const remaining = s.slice(1);
-    const chars = [s[0]];
-    return { remaining, chars };
-  }
-};
+// const getOptions = (s) => {
+//   if (s[0] === "(") {
+//     const i = s.indexOf(")");
+//     const remaining = s.slice(i + 1);
+//     const chars = s.slice(1, i).split("");
+//     return { remaining, chars };
+//   } else {
+//     const remaining = s.slice(1);
+//     const chars = [s[0]];
+//     return { remaining, chars };
+//   }
+// };
 
 module.exports = {
   parentheticalPossibilities,
