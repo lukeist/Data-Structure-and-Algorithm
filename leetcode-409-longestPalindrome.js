e0a3083f48fcfc4df39e8e8a8a1189b3642f3062
hiluan <apt.muenchen@gmail.com>
2023-01-20 19:41:31 -0800

    hashmap O(n) O(n)

---
+++ leetcode-409-longestPalindrome.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// p: str
+// r: num
+// e:
+// abccccdd
+// dccaccd
+//                                                           abccccdd
+//                                                           11 4  2
+//
+var longestPalindrome = function (s) {
+  let map = {};
+  for (let c of s) {
+    map[c] = (map[c] || 0) + 1;
+  }
+  let count = 0;
+  for (let k in map) {
+    count += !(map[k] % 2) ? map[k] : map[k] - 1;
+  }
+  return s.length > count ? count + 1 : count;
+};
+
+// s.length > count: a ab aaa aaab aaabbb
+// s.length === count: aa aabb aaaabb
+
+console.log(longestPalindrome("abccccdd"));
