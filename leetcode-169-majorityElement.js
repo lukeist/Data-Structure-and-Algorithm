6973e4929575ffea6f1367e2bc7fa62abb644716
hiluan <apt.muenchen@gmail.com>
2023-04-10 01:33:51 -0700

    array - Boyer-Moore Majority Vote Algorithm O(n) O(1)

--- leetcode-169-majorityElement.js
+++ leetcode-169-majorityElement.js
@@ -1,21 +1,39 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+function majorityElement(nums) {
+  let majority = nums[0];
+  let count = 1;
 
-// p: arr
-// r: num
-// 2,2,1,1,1,2,2
-// 2:
-// 1:
-var majorityElement = function (nums) {
-  const n = nums.length;
-  const map = {};
-  for (let num of nums) {
-    map[num] = (map[num] || 0) + 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === majority) {
+      count++;
+    } else {
+      count--;
+      if (count === 0) {
+        majority = nums[i];
+        count = 1;
+      }
+    }
   }
 
-  for (let k in map) {
-    if (map[k] > n / 2) return k;
-  }
-};
+  return majority;
+}
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+
+// // p: arr
+// // r: num
+// // 2,2,1,1,1,2,2
+// // 2:
+// // 1:
+// var majorityElement = function (nums) {
+//   const n = nums.length;
+//   const map = {};
+//   for (let num of nums) {
+//     map[num] = (map[num] || 0) + 1;
+//   }
+
+//   for (let k in map) {
+//     if (map[k] > n / 2) return k;
+//   }
+// };
