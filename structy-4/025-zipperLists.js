e91173129e369e225894a8a7455b96febb42e206
hiluan <apt.muenchen@gmail.com>
2023-09-21 17:02:14 -0700

    linked list - iter O(n) O(1) + recu O(n)

--- structy-4/025-zipperLists.js
+++ structy-4/025-zipperLists.js
@@ -5,6 +5,50 @@
 //   }
 // }
 
+// p: head of ll, head of ll;
+// r: head of ll;
+// e:
+//                c1  n1
+//      a    b   c
+//      | /  | /
+//      x   y
+//               c2   n2
+// 1
+///////////////////////////////////////////////// BEST solutions:
+// const zipperLists = (head1, head2) => {
+//   let c1 = head1, c2 = head2, count = 1;
+
+//   while (!!c1 && !!c2) {
+//     if (count % 2 !== 0) {
+//       let n1 = c1.next;
+//       c1.next = c2;
+//       c1 = n1;
+//     } else {
+//       let n2 = c2.next;
+//       c2.next = c1;
+//       c2 = n2;
+//     }
+//     count++;
+//   }
+//   return head1;
+// };
+
+const zipperLists = (head1, head2) => {
+  if (!head1) return head2;
+  if (!head2) return head1;
+
+  head1.next = zipperLists(head2, head1.next);
+  return head1;
+};
+
+///////////////////////////////////////////////// ok solutions:
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
 // p: head ll, head ll;
 // r: head ll;
 // e:
@@ -67,14 +111,14 @@
 //   return head1;
 // }
 
-const zipperLists = (head1, head2) => {
-  if (!head1) return head2;
-  if (!head2) return head1;
-  let n1 = head1.next;
-  let n2 = head2.next;
+// const zipperLists = (head1, head2) => {
+//   if (!head1) return head2;
+//   if (!head2) return head1;
+//   let n1 = head1.next;
+//   let n2 = head2.next;
 
-  head1.next = head2;
-  head2.next = zipperLists(n1, n2);
+//   head1.next = head2;
+//   head2.next = zipperLists(n1, n2);
 
-  return head1;
-};
+//   return head1;
+// };
