74ba443d7f5e4b67655dab5d42481d06159fca44
hiluan <apt.muenchen@gmail.com>
2024-02-28 17:37:35 -0800

    string - O(n^2) O(1)

---
+++ oa/ebay/!!!patternForAGivenString.js
@@ -0,0 +1,66 @@
+// Can you solve this problem.
+// Write a program to return the pattern for a given string like below:
+// If string is aaaaa, pattern should be a
+// Example:
+
+// 01234
+//  ik
+// aaaaa -> a
+//    j
+// pattern: a
+// sub: a
+// isValid = 1
+
+// ababab -> ab
+
+// 0123456789
+// abcababcab -> abcab
+//      i
+// abcdabcabcdabc -> abcdabc.
+//
+//
+// 0123456
+//        i
+// abcdefg -> throw Exception / return ""
+//        j
+// pattern: abcdefg
+// sub: abcab
+// isValid = 1
+
+// abcdefgabc -> throw Exception / return ""
+
+// p: string
+// r: string
+// 0123456789
+//      i    k
+// abcababcab -> abcab
+//      j
+// pattern: abcab
+// sub: abcab
+// isValid = 1
+
+const patternForAGivenString = (s) => {
+  for (let i = 1; i <= s.length / 2; i++) {
+    let patt = s.substring(0, i);
+    let isValid = true;
+
+    for (let j = i; j < s.length; j += i) {
+      let sub = s.substring(j, j + i);
+
+      if (patt !== sub) {
+        isValid = false;
+        break;
+      }
+    }
+
+    if (isValid) return patt;
+  }
+  return null;
+};
+
+console.log(patternForAGivenString("aaaaa"));
+console.log(patternForAGivenString("ababab"));
+console.log(patternForAGivenString("abcababcab"));
+console.log(patternForAGivenString("abcdabcabcdabc"));
+console.log(patternForAGivenString("abcdefg"));
+console.log(patternForAGivenString("aaaaabcaaaaabc"));
---
+++ oa/ebay/findNumberOfIndices.js
@@ -0,0 +1,22 @@
+// You are given three arrays, a, b, and queries.
+// Each query can be formatted in two ways,
+// [0,i,x] or [1,x].
+
+// If the query is [0,i,x]:
+// You must add x to the value at b[i]
+
+// If the query is [1,x]:
+// You must find the number of indices i and j such that a[i]+b[j] == x
+
+// return an array of all the outputs you get from queries in the form [1,x]
+//
+//
+//
+//
+//
+//
+// a = []
+// b = []
+//
+// [[0,i,x] or [1,x]]
+//
