b2ecc86dd371dadcc1ed71962261df48d7d43cd7
hiluan <apt.muenchen@gmail.com>
2024-03-13 14:51:18 -0700

    uber interview question

---
+++ interviews-oa/uber/rateLimiter.js
@@ -0,0 +1,5 @@
+// Recently this question was asked in Uber Onsite. Can someone please help solving this question?
+// Every request comes in with a customerID, implement this method boolean rateLimit(int customerId).
+// Allow X requests in Y seconds.
+
+const rateLimit = (id) => {};
