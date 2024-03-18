8d4d5c6d4fdde5b3c63d6083578d07e88449dc2b
hiluan <apt.muenchen@gmail.com>
2024-03-18 15:59:06 -0700

    doubly linked list - O(1)/O(n) O(1)/O(n)

--- interviews-oa/google/recentSearches.js
+++ interviews-oa/google/recentSearches.js
@@ -6,42 +6,124 @@
 
 // p: number int,
 // r:
+//
+// head <-> a <-> b <-> c <-> tail
+//
 
-// [a,b,c]
-// [b,c,d]
-// [b,c,d]
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
 
-class RecentSearches {
-  constructor(cap) {
-    this.cap = cap;
-    this.searches = [];
+class Dll {
+  constructor() {
+    this.head = new Node();
+    this.tail = new Node();
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
   }
 
-  search(string) {
-    if (this.searches.length === this.cap) {
-      if (this.searches.includes(string)) {
-        const i = this.searches.indexOf(string);
-        this.searches.splice(i, 1);
-        this.searches.push(string);
-      } else {
-        this.searches.shift();
-        this.searches.push(string);
-      }
-    } else if (this.searches.length >= 0) {
-      this.searches.push(string);
+  insertTail(node) {
+    const c = this.tail.prev;
+    c.next = node;
+    node.prev = c;
+    node.next = this.tail;
+    this.tail.prev = node;
+  }
+
+  removeNode(node) {
+    const p = node.prev;
+    const n = node.next;
+    p.next = n;
+    n.prev = p;
+  }
+
+  moveToTail(node) {
+    this.removeNode(node);
+    this.insertTail(node);
+  }
+
+  removeHead() {
+    this.removeNode(head.next);
+    return this.head.next.key;
+  }
+}
+
+class recentSearches {
+  constructor(cap) {
+    this.cap = cap;
+    this.size = size;
+    this.searches = {};
+    this.dll = new Dll();
+  }
+
+  getRecent() {
+    const result = [];
+
+    // note: for more accurate order- it should be traverse thru the linked list instead of this:
+    for (const [key, val] of Object.entries(this.searches)) {
+      result.push(val);
+    }
+    return result;
+  }
+
+  putSearch(string) {
+    if (!(string in this.searches)) {
+      const node = new Node(string, null);
+      this.searches[string] = node;
+      this.dll.insertTail(node);
+
+      if (this.cap === this.size) {
+        const key = this.dll.removeHead();
+        delete this.searches[key];
+      } else this.size++;
+    } else {
+      this.dll.moveToTail(this.searches[string]);
     }
   }
 }
 
-const s = new RecentSearches(3);
+//
+//
+// [a,b,c]
+// [b,c,d]
+// [b,c,d]
 
-s.search("1");
-console.log(s.searches);
-s.search("2");
-console.log(s.searches);
-s.search("3");
-console.log(s.searches);
-s.search("4");
-console.log(s.searches);
-s.search("3");
-console.log(s.searches);
+// class RecentSearches {
+//   constructor(cap) {
+//     this.cap = cap;
+//     this.searches = [];
+//   }
+
+//   search(string) {
+//     if (this.searches.length === this.cap) {
+//       if (this.searches.includes(string)) {
+//         const i = this.searches.indexOf(string);
+//         this.searches.splice(i, 1);
+//         this.searches.push(string);
+//       } else {
+//         this.searches.shift();
+//         this.searches.push(string);
+//       }
+//     } else if (this.searches.length >= 0) {
+//       this.searches.push(string);
+//     }
+//   }
+// }
+
+// const s = new RecentSearches(3);
+
+// s.search("1");
+// console.log(s.searches);
+// s.search("2");
+// console.log(s.searches);
+// s.search("3");
+// console.log(s.searches);
+// s.search("4");
+// console.log(s.searches);
+// s.search("3");
+// console.log(s.searches);
